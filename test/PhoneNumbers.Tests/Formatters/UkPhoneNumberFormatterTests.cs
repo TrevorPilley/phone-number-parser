e953f3ce277dd09b652ece3788571ed313ab2f9c
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-12-20 21:12:44 +0000

    Simplified UkPhoneNumberFormatter tests

--- test/PhoneNumbers.Tests/Formatters/UkPhoneNumberFormatterTests.cs
+++ test/PhoneNumbers.Tests/Formatters/UkPhoneNumberFormatterTests.cs
@@ -13,33 +13,18 @@ namespace PhoneNumbers.Tests.Formatters
     public class UkPhoneNumberFormatterTests
     {
         [Theory]
-        [InlineData("01132224444", "0113 222 4444")]
-        [InlineData("01142734567", "0114 273 4567")]
-        [InlineData("01159155555", "0115 915 5555")]
-        [InlineData("01164541002", "0116 454 1002")]
-        [InlineData("01179222000", "0117 922 2000")]
-        [InlineData("01189373787", "0118 937 3787")]
-        public void Format_11X_Display(string value, string expected) =>
+        [InlineData("01132224444", "0113 222 4444")] // 11X
+        public void Format_Display(string value, string expected) =>
             Assert.Equal(expected, new UkPhoneNumberFormatter().Format(PhoneNumber.Parse("GB", value), "D"));
 
         [Theory]
-        [InlineData("01132224444", "+441132224444")]
-        [InlineData("01142734567", "+441142734567")]
-        [InlineData("01159155555", "+441159155555")]
-        [InlineData("01164541002", "+441164541002")]
-        [InlineData("01179222000", "+441179222000")]
-        [InlineData("01189373787", "+441189373787")]
-        public void Format_11X_International(string value, string expected) =>
+        [InlineData("01132224444", "+441132224444")] // 11X
+        public void Format_International(string value, string expected) =>
             Assert.Equal(expected, new UkPhoneNumberFormatter().Format(PhoneNumber.Parse("GB", value), "I"));
 
         [Theory]
-        [InlineData("01132224444", "01132224444")]
-        [InlineData("01142734567", "01142734567")]
-        [InlineData("01159155555", "01159155555")]
-        [InlineData("01164541002", "01164541002")]
-        [InlineData("01179222000", "01179222000")]
-        [InlineData("01189373787", "01189373787")]
-        public void Format_11X_National(string value, string expected) =>
+        [InlineData("01132224444", "01132224444")] // 11X
+        public void Format_National(string value, string expected) =>
             Assert.Equal(expected, new UkPhoneNumberFormatter().Format(PhoneNumber.Parse("GB", value), "N"));
     }
 }
