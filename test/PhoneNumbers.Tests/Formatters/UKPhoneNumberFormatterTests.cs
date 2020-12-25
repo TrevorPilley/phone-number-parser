4d6d0791efb3ef5c6cd48a1d79af8a1b2c5e7f95
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-12-25 09:23:21 +0000

    Update UKPhoneNumberFormatterTests.cs

--- test/PhoneNumbers.Tests/Formatters/UKPhoneNumberFormatterTests.cs
+++ test/PhoneNumbers.Tests/Formatters/UKPhoneNumberFormatterTests.cs
@@ -18,7 +18,7 @@ namespace PhoneNumbers.Tests.Formatters
         [InlineData("01733112233", "01733 112 233")] // 1XXX
         [InlineData("02076416000", "020 7641 6000")] // 2X
         [InlineData("03001212123", "0300 121 2123")] // 3XX
-        [InlineData("07106112233", "07106 112233")]  // 7X
+        [InlineData("07106112233", "07106 112233")]  // 7XXX
         [InlineData("0800121121", "0800 121 121")] // 8XX
         public void Format_Display(string value, string expected) =>
             Assert.Equal(expected, new UKPhoneNumberFormatter().Format(PhoneNumber.Parse(value, "GB"), "D"));
@@ -29,7 +29,7 @@ namespace PhoneNumbers.Tests.Formatters
         [InlineData("01733112233", "+441733112233")] // 1XXX
         [InlineData("02076416000", "+442076416000")] // 2X
         [InlineData("03001212123", "+443001212123")] // 3XX
-        [InlineData("07106112233", "+447106112233")] // 7X
+        [InlineData("07106112233", "+447106112233")] // 7XXX
         [InlineData("0800121121", "+44800121121")] // 8XX
         public void Format_International(string value, string expected) =>
             Assert.Equal(expected, new UKPhoneNumberFormatter().Format(PhoneNumber.Parse(value, "GB"), "I"));
@@ -40,7 +40,7 @@ namespace PhoneNumbers.Tests.Formatters
         [InlineData("01733112233", "01733112233")] // 1XXX
         [InlineData("02076416000", "02076416000")] // 2X
         [InlineData("03001212123", "03001212123")] // 3XX
-        [InlineData("07106112233", "07106112233")] // 7X
+        [InlineData("07106112233", "07106112233")] // 7XXX
         [InlineData("0800121121", "0800121121")] // 8XX
         public void Format_National(string value, string expected) =>
             Assert.Equal(expected, new UKPhoneNumberFormatter().Format(PhoneNumber.Parse(value, "GB"), "N"));
