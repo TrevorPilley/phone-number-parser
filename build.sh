e592156879b034295a80b62b966bbd68836af7b3
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2020-12-31 17:57:16 +0000

    Build locally in release and re-order to ensure package is created

--- build.sh
+++ build.sh
@@ -5,11 +5,10 @@ rm -r test/PhoneNumbers.Tests/TestResults/
 dotnet tool update --global dotnet-reportgenerator-globaltool
 
 dotnet clean --verbosity minimal
-dotnet build --verbosity minimal
+dotnet build --configuration Release --verbosity minimal
 dotnet test --no-restore --verbosity minimal /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./TestResults/
+dotnet pack --no-build --configuration Release
 
 cd test/PhoneNumbers.Tests
 reportgenerator "-reports:./TestResults/coverage.cobertura.xml" "-targetdir:./TestResults/Coverage" -reporttypes:HTML
 open "./TestResults/Coverage/index.htm"
-
-dotnet pack --no-build
