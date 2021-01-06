56f8a1222a1826bce324c24ef9299a5cff52362d
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-06 21:53:20 +0000

    Update TestHelper.cs

--- test/PhoneNumbers.Tests/TestHelper.cs
+++ test/PhoneNumbers.Tests/TestHelper.cs
@@ -4,12 +4,11 @@ namespace PhoneNumbers.Tests
 {
     internal static class TestHelper
     {
-        internal static CountryInfo CreateCountryInfo(bool hasAreaCodes, int[] areaCodeLengths, int[] nsnLengths) =>
+        internal static CountryInfo CreateCountryInfo(int[] areaCodeLengths, int[] nsnLengths) =>
             new()
             {
                 AreaCodeLengths = new ReadOnlyCollection<int>(areaCodeLengths),
                 CallingCode = "+422", // +422 isn't a used calling code.
-                HasAreaCodes = hasAreaCodes,
                 Iso3116Code = "ZZ", // ZZ isn't a used ISO 3116 code.
                 NsnLengths = new ReadOnlyCollection<int>(nsnLengths),
             };
