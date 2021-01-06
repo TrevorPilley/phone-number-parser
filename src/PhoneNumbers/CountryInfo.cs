4302190d560b895f711834ad71613465c75c793a
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-06 21:25:35 +0000

    Add the CountryInfo for France

--- src/PhoneNumbers/CountryInfo.cs
+++ src/PhoneNumbers/CountryInfo.cs
@@ -22,6 +22,17 @@ namespace PhoneNumbers
         }
 
         /// <summary>
+        /// Gets the <see cref="CountryInfo"/> for the France.
+        /// </summary>
+        public static CountryInfo FR { get; } = new()
+        {
+            CallingCode = "+33",
+            HasAreaCodes = false,
+            Iso3116Code = "FR",
+            NsnLengths = new ReadOnlyCollection<int>(new[] { 9 }),
+        };
+
+        /// <summary>
         /// Gets the <see cref="CountryInfo"/> for Ireland.
         /// </summary>
         /// <remarks>Covers the Republic of Ireland, Northern Ireland is part of the United Kingdom.</remarks>
