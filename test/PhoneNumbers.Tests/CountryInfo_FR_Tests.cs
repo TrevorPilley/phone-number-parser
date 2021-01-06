21c48b4cf08aee07a676d7c4c970f4aed51efa4d
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-06 21:32:15 +0000

    Create CountryInfo_FR_Tests.cs

---
+++ test/PhoneNumbers.Tests/CountryInfo_FR_Tests.cs
@@ -0,0 +1,25 @@
+using PhoneNumbers.Formatters;
+using Xunit;
+
+namespace PhoneNumbers.Tests
+{
+    public class CountryInfo_FR_Tests
+    {
+        [Fact]
+        public void FR()
+        {
+            Assert.Same(CountryInfo.FR, CountryInfo.FR);
+
+            var countryInfo = CountryInfo.FR;
+
+            Assert.Empty(countryInfo.AreaCodeLengths);
+            Assert.Equal("+33", countryInfo.CallingCode);
+            Assert.IsType<DefaultPhoneNumberFormatter>(countryInfo.Formatter);
+            Assert.False(countryInfo.HasAreaCodes);
+            Assert.Equal("00", countryInfo.InternationalCallPrefix);
+            Assert.Equal("FR", countryInfo.Iso3116Code);
+            Assert.Equal(new[] { 9 }, countryInfo.NsnLengths);
+            Assert.Equal("0", countryInfo.TrunkPrefix);
+        }
+    }
+}
