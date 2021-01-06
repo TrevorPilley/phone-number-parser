daaa416c8605e10383caf5ab7b9c737741a2c079
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-06 22:00:03 +0000

    Update CountryInfo_FR_Tests.cs

--- test/PhoneNumbers.Tests/CountryInfo_FR_Tests.cs
+++ test/PhoneNumbers.Tests/CountryInfo_FR_Tests.cs
@@ -21,5 +21,38 @@ namespace PhoneNumbers.Tests
             Assert.Equal(new[] { 9 }, countryInfo.NsnLengths);
             Assert.Equal("0", countryInfo.TrunkPrefix);
         }
+
+        [Theory]
+        [InlineData(default(string))]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("+35312222222")]
+        public void FR_IsInternationalNumber_False(string value) =>
+            Assert.False(CountryInfo.FR.IsInternationalNumber(value));
+
+        [Theory]
+        [InlineData("+33122334455")]
+        [InlineData("+33(0)122334455")]
+        [InlineData("+33 (0)1 22 33 44 55")]
+        public void FR_IsInternationalNumber_True(string value) =>
+            Assert.True(CountryInfo.FR.IsInternationalNumber(value));
+
+        [Theory]
+        [InlineData("+33122334455")]
+        [InlineData("+33(0)122334455")]
+        [InlineData("+33 (0)1 22 33 44 55")]
+        [InlineData("(01) 22 33 44 55")]
+        [InlineData("0122334455")]
+        [InlineData("01-22-33-44-55")]
+        public void FR_ReadNationalSignificantNumber(string value) =>
+            Assert.Equal("122334455", CountryInfo.FR.ReadNationalSignificantNumber(value));
+
+        [Theory]
+        [InlineData(default(string))]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("+35312222222")]
+        public void FR_ReadNationalSignificantNumber_Not_A_Valid_Value(string value) =>
+            Assert.Equal(string.Empty, CountryInfo.FR.ReadNationalSignificantNumber(value));
     }
 }
