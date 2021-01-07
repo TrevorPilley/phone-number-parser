ced8abdd770420a4986d9855d8b609fc7585b023
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-07 16:59:08 +0000

    Build locally in debug mode

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ rm -r test/PhoneNumbers.Tests/TestResults/
 dotnet tool update --global dotnet-reportgenerator-globaltool
 
 dotnet clean --verbosity minimal
-dotnet build --configuration Release --verbosity minimal
+dotnet build --verbosity minimal
 dotnet test --no-restore --verbosity minimal /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./TestResults/
-dotnet pack --no-build --configuration Release
+dotnet pack --no-build
 
 cd test/PhoneNumbers.Tests
 reportgenerator "-reports:./TestResults/coverage.cobertura.xml" "-targetdir:./TestResults/Coverage" -reporttypes:HTML
