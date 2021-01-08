d1164da48d5e0f8febc39a0f938db6137eceeafc
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-08 11:12:24 +0000

    Change Iso3116Code to Iso3166Code

--- test/PhoneNumbers.Tests/CountryInfo_HK_Tests.cs
+++ test/PhoneNumbers.Tests/CountryInfo_HK_Tests.cs
@@ -17,7 +17,7 @@ namespace PhoneNumbers.Tests
             Assert.IsType<HKPhoneNumberFormatter>(countryInfo.Formatter);
             Assert.False(countryInfo.HasAreaCodes);
             Assert.Equal("001", countryInfo.InternationalCallPrefix);
-            Assert.Equal("HK", countryInfo.Iso3116Code);
+            Assert.Equal("HK", countryInfo.Iso3166Code);
             Assert.Equal(new[] { 8, 9 }, countryInfo.NsnLengths);
             Assert.Null(countryInfo.TrunkPrefix);
         }
