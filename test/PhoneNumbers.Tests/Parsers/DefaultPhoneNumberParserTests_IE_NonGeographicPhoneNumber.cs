2bd2206047003b21367a82fb3184ebb1e165693d
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-10 20:05:15 +0000

    Create DefaultPhoneNumberParserTests_IE_NonGeographicPhoneNumber.cs

---
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_IE_NonGeographicPhoneNumber.cs
@@ -0,0 +1,30 @@
+using PhoneNumbers.Parsers;
+using Xunit;
+
+namespace PhoneNumbers.Tests.Parsers
+{
+    /// <summary>
+    /// Contains unit tests for the <see cref="DefaultPhoneNumberParser"/> class for IE <see cref="NonGeographicPhoneNumber"/>s.
+    /// </summary>
+    public class DefaultPhoneNumberParserTests_IE_NonGeographicPhoneNumber
+    {
+        private readonly PhoneNumberParser _parser = DefaultPhoneNumberParser.Create(CountryInfo.IE);
+
+        [Theory]
+        [InlineData("0800000000", "800", "000000")]
+        [InlineData("0800999999", "800", "999999")]
+        public void Parse_Known_NonGeographicPhoneNumber_FreePhone(string value, string areaCode, string localNumber)
+        {
+            var phoneNumber = _parser.Parse(value).PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+            var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+            Assert.Equal(areaCode, nonGeographicPhoneNumber.AreaCode);
+            Assert.Equal(CountryInfo.IE, nonGeographicPhoneNumber.Country);
+            Assert.True(nonGeographicPhoneNumber.IsFreephone);
+            Assert.Equal(localNumber, nonGeographicPhoneNumber.LocalNumber);
+        }
+    }
+}
