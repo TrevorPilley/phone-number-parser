75643b93eabf5d84e985664db886bb5050423274
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-13 06:57:25 +0000

    Update CountryInfo_IT_Tests.cs

--- test/PhoneNumbers.Tests/CountryInfo_IT_Tests.cs
+++ test/PhoneNumbers.Tests/CountryInfo_IT_Tests.cs
@@ -14,7 +14,7 @@ namespace PhoneNumbers.Tests
 
             Assert.Equal(new[] { 4, 3, 2 }, countryInfo.AreaCodeLengths);
             Assert.Equal("+39", countryInfo.CallingCode);
-            Assert.IsType<PhoneNumberFormatter>(countryInfo.Formatter);
+            Assert.IsType<ITPhoneNumberFormatter>(countryInfo.Formatter);
             Assert.True(countryInfo.HasAreaCodes);
             Assert.Equal("00", countryInfo.InternationalCallPrefix);
             Assert.Equal("IT", countryInfo.Iso3166Code);
