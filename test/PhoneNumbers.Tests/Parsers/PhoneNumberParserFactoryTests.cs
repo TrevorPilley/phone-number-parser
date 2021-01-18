6cf4f08b405b83ac1c18546825371763e52aa0ee
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-18 15:50:57 +0000

    Update PhoneNumberParserFactoryTests.cs

--- test/PhoneNumbers.Tests/Parsers/PhoneNumberParserFactoryTests.cs
+++ test/PhoneNumbers.Tests/Parsers/PhoneNumberParserFactoryTests.cs
@@ -11,6 +11,10 @@ namespace PhoneNumbers.Tests.Parsers
         private readonly PhoneNumberParserFactory _factory = new();
 
         [Fact]
+        public void GetParser_For_CountryInfo_ES_Returns_DefaultPhoneNumberParser() =>
+            Assert.IsType<DefaultPhoneNumberParser>(_factory.GetParser(CountryInfo.ES));
+
+        [Fact]
         public void GetParser_For_CountryInfo_FR_Returns_DefaultPhoneNumberParser() =>
             Assert.IsType<DefaultPhoneNumberParser>(_factory.GetParser(CountryInfo.FR));
 
