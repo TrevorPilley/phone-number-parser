acbc39b4853d55566e44a1362c148d25ea70c2e4
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-22 09:37:40 +0000

    Create DefaultPhoneNumberParserTests_SG_MobilePhoneNumber.cs

---
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_SG_MobilePhoneNumber.cs
@@ -0,0 +1,37 @@
+using PhoneNumbers.Parsers;
+using Xunit;
+
+namespace PhoneNumbers.Tests.Parsers
+{
+    /// <summary>
+    /// Contains unit tests for the <see cref="DefaultPhoneNumberParser"/> class for SG <see cref="PhoneNumber"/>s.
+    /// </summary>
+    public class DefaultPhoneNumberParserTests_SG_MobilePhoneNumber
+    {
+        private readonly PhoneNumberParser _parser = DefaultPhoneNumberParser.Create(CountryInfo.SG);
+
+        [Theory]
+        [InlineData("81000000", "81000000")]
+        [InlineData("89999999", "89999999")]
+        [InlineData("90000000", "90000000")]
+        [InlineData("98999999", "98999999")]
+        public void Parse_Known_MobilePhoneNumber(string value, string localNumber)
+        {
+            var parseResult = _parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+            var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+            Assert.Null(mobilePhoneNumber.AreaCode);
+            Assert.Equal(CountryInfo.SG, mobilePhoneNumber.Country);
+            Assert.False(mobilePhoneNumber.IsDataOnly);
+            Assert.False(mobilePhoneNumber.IsPager);
+            Assert.False(mobilePhoneNumber.IsVirtual);
+            Assert.Equal(localNumber, mobilePhoneNumber.LocalNumber);
+        }
+    }
+}
