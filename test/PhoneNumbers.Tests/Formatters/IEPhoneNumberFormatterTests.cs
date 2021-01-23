6d94a3b9d3ec981fdfe43208d001ec10439d9978
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-23 18:21:30 +0000

    Only test the display method
    Since it's the only one overriden

--- test/PhoneNumbers.Tests/Formatters/IEPhoneNumberFormatterTests.cs
+++ test/PhoneNumbers.Tests/Formatters/IEPhoneNumberFormatterTests.cs
@@ -1,4 +1,3 @@
-using System;
 using PhoneNumbers.Formatters;
 using Xunit;
 
@@ -17,23 +16,5 @@ namespace PhoneNumbers.Tests.Formatters
         [InlineData("040226969", "0402 26969")]
         public void Format_Display(string value, string expected) =>
             Assert.Equal(expected, _formatter.Format(PhoneNumber.Parse(value, "IE"), "D"));
-
-        [Theory]
-        [InlineData("012222222", "+35312222222")]
-        [InlineData("0214924000", "+353214924000")]
-        [InlineData("040226969", "+35340226969")]
-        public void Format_International(string value, string expected) =>
-            Assert.Equal(expected, _formatter.Format(PhoneNumber.Parse(value, "IE"), "I"));
-
-        [Theory]
-        [InlineData("012222222", "012222222")]
-        [InlineData("0214924000", "0214924000")]
-        [InlineData("040226969", "040226969")]
-        public void Format_National(string value, string expected) =>
-            Assert.Equal(expected, _formatter.Format(PhoneNumber.Parse(value, "IE"), "N"));
-
-        [Fact]
-        public void Format_Throws_Exception_For_Invalid_Format() =>
-            Assert.Throws<FormatException>(() => _formatter.Format(PhoneNumber.Parse("+441132224444"), "C"));
     }
 }
