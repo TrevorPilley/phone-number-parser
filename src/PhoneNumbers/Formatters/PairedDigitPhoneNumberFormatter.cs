ab8a12041164e9a446f57e3b3ca72982988b7f05
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-25 10:50:56 +0000

    Add a paired digit formatter
    It can replace the France and Monaco formatters catering with and without a trunk code and area code.

---
+++ src/PhoneNumbers/Formatters/PairedDigitPhoneNumberFormatter.cs
@@ -0,0 +1,51 @@
+using System.Text;
+
+namespace PhoneNumbers.Formatters
+{
+    /// <summary>
+    /// A <see cref="PhoneNumberFormatter"/> for phone numbers where the display format is 'xx xx xx xx',
+    /// considering trunk prefix, area code and local number.
+    /// </summary>
+    internal sealed class PairedDigitPhoneNumberFormatter : PhoneNumberFormatter
+    {
+        private PairedDigitPhoneNumberFormatter()
+        {
+        }
+
+        internal static PhoneNumberFormatter Instance { get; } = new PairedDigitPhoneNumberFormatter();
+
+        /// <inheritdoc/>
+        protected override string FormatDisplay(PhoneNumber phoneNumber)
+        {
+            var stringBuilder = new StringBuilder();
+
+            var charsAdded = 0;
+
+            static void Append(StringBuilder sb, ref int charsAdded, string? input)
+            {
+                for (int i = 0; i < input?.Length; i++)
+                {
+                    sb.Append(input[i]);
+
+                    charsAdded++;
+
+                    if (charsAdded % 2 == 0)
+                    {
+                        sb.Append(' ');
+                    }
+                }
+            }
+
+            Append(stringBuilder, ref charsAdded, phoneNumber.Country.TrunkPrefix);
+            Append(stringBuilder, ref charsAdded, phoneNumber.AreaCode);
+            Append(stringBuilder, ref charsAdded, phoneNumber.LocalNumber);
+
+            if (stringBuilder[stringBuilder.Length - 1] == ' ')
+            {
+                stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
