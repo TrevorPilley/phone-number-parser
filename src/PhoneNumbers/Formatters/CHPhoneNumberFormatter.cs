8a55a0c28104379ba39210af241914a268386187
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-28 10:59:55 +0000

    Create CHPhoneNumberFormatter.cs

---
+++ src/PhoneNumbers/Formatters/CHPhoneNumberFormatter.cs
@@ -0,0 +1,19 @@
+namespace PhoneNumbers.Formatters
+{
+    /// <summary>
+    /// A <see cref="PhoneNumberFormatter"/> for CH phone numbers.
+    /// </summary>
+    internal sealed class CHPhoneNumberFormatter : PhoneNumberFormatter
+    {
+        /// <inheritdoc/>
+        protected override string FormatDisplay(PhoneNumber phoneNumber)
+        {
+            if (phoneNumber.AreaCode.Length = 3)
+            {
+                return $"{phoneNumber.AreaCode} {phoneNumber.LocalNumber.Substring(0, 3)} {phoneNumber.LocalNumber.Substring(3)}";
+            }
+
+            return $"{phoneNumber.AreaCode}{phoneNumber.LocalNumber.Substring(0, 1)} {phoneNumber.LocalNumber.Substring(1, 3) {phoneNumber.LocalNumber.Substring(4)}";
+        }
+    }
+}
