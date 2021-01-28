905340f626a5c0ec2b911423b8cd5135a8a9f0bf
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-01-28 11:03:36 +0000

    Create CHPhoneNumberFormatterTests.cs

---
+++ test/PhoneNumbers.Tests/Formatters/CHPhoneNumberFormatterTests.cs
@@ -0,0 +1,18 @@
+using PhoneNumbers.Formatters;
+using Xunit;
+
+namespace PhoneNumbers.Tests.Formatters
+{
+    /// <summary>
+    /// Contains unit tests for the <see cref="CHPhoneNumberFormatter"/> class.
+    /// </summary>
+    public class CHPhoneNumberFormatterTests
+    {
+        private readonly PhoneNumberFormatter _formatter = new CHPhoneNumberFormatter();
+
+        [Theory]
+        [InlineData("0327654321", "032 765 43 21")]
+        public void Format_Display(string value, string expected) =>
+            Assert.Equal(expected, _formatter.Format(PhoneNumber.Parse(value, "CH"), "D"));
+    }
+}
