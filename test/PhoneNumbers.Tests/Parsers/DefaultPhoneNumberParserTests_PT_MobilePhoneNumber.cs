39623d573177eb45cc56ab0934832a254d51f2c0
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-02-04 14:39:34 +0000

    Portugal numbers

---
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_PT_MobilePhoneNumber.cs
@@ -0,0 +1,61 @@
+using PhoneNumbers.Parsers;
+using Xunit;
+
+namespace PhoneNumbers.Tests.Parsers
+{
+    /// <summary>
+    /// Contains unit tests for the <see cref="DefaultPhoneNumberParser"/> class for PT <see cref="PhoneNumber"/>s.
+    /// </summary>
+    public class DefaultPhoneNumberParserTests_PT_MobilePhoneNumber
+    {
+        private readonly PhoneNumberParser _parser = DefaultPhoneNumberParser.Create(CountryInfo.Portugal);
+
+        [Theory]
+        [InlineData("900000000", "900", "000000")]
+        [InlineData("900999999", "900", "999999")]
+        [InlineData("903000000", "903", "000000")]
+        [InlineData("903999999", "903", "999999")]
+        [InlineData("906000000", "906", "000000")]
+        [InlineData("906999999", "906", "999999")]
+        public void Parse_Known_MobilePhoneNumber(string value, string NationalDestinationCode, string subscriberNumber)
+        {
+            var parseResult = _parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+            var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+            Assert.Equal(CountryInfo.Portugal, mobilePhoneNumber.Country);
+            Assert.False(mobilePhoneNumber.IsDataOnly);
+            Assert.False(mobilePhoneNumber.IsPager);
+            Assert.False(mobilePhoneNumber.IsVirtual);
+            Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+            Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+        }
+
+        [Theory]
+        [InlineData("884000000", "884", "000000")]
+        [InlineData("884999999", "884", "999999")]
+        public void Parse_Known_MobilePhoneNumber_Virtual(string value, string NationalDestinationCode, string subscriberNumber)
+        {
+            var parseResult = _parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+            var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+            Assert.Equal(CountryInfo.Portugal, mobilePhoneNumber.Country);
+            Assert.False(mobilePhoneNumber.IsDataOnly);
+            Assert.False(mobilePhoneNumber.IsPager);
+            Assert.True(mobilePhoneNumber.IsVirtual);
+            Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+            Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+        }
+    }
+}
---
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_PT_NonGeographicPhoneNumber.cs
@@ -0,0 +1,121 @@
+using PhoneNumbers.Parsers;
+using Xunit;
+
+namespace PhoneNumbers.Tests.Parsers
+{
+    /// <summary>
+    /// Contains unit tests for the <see cref="DefaultPhoneNumberParser"/> class for PT <see cref="PhoneNumber"/>s.
+    /// </summary>
+    public class DefaultPhoneNumberParserTests_PT_NonGeographicPhoneNumber
+    {
+        private readonly PhoneNumberParser _parser = DefaultPhoneNumberParser.Create(CountryInfo.Portugal);
+
+        [Theory]
+        [InlineData("300000000", "300", "000000")]
+        [InlineData("300999999", "300", "999999")]
+        [InlineData("399000000", "399", "000000")]
+        [InlineData("399999999", "399", "999999")]
+        public void Parse_Known_NonGeographicPhoneNumber_3XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+        {
+            var parseResult = _parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+            var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+            Assert.Equal(CountryInfo.Portugal, nonGeographicPhoneNumber.Country);
+            Assert.False(nonGeographicPhoneNumber.IsFreephone);
+            Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+            Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+        }
+
+        [Theory]
+        [InlineData("607000000", "607", "000000")]
+        [InlineData("607999999", "607", "999999")]
+        public void Parse_Known_NonGeographicPhoneNumber_6XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+        {
+            var parseResult = _parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+            var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+            Assert.Equal(CountryInfo.Portugal, nonGeographicPhoneNumber.Country);
+            Assert.False(nonGeographicPhoneNumber.IsFreephone);
+            Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+            Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+        }
+
+        [Theory]
+        [InlineData("700000000", "700", "000000")]
+        [InlineData("700999999", "700", "999999")]
+        [InlineData("799000000", "799", "000000")]
+        [InlineData("799999999", "799", "999999")]
+        public void Parse_Known_NonGeographicPhoneNumber_7XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+        {
+            var parseResult = _parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+            var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+            Assert.Equal(CountryInfo.Portugal, nonGeographicPhoneNumber.Country);
+            Assert.False(nonGeographicPhoneNumber.IsFreephone);
+            Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+            Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+        }
+
+        [Theory]
+        [InlineData("808000000", "808", "000000")]
+        [InlineData("808999999", "808", "999999")]
+        [InlineData("809000000", "809", "000000")]
+        [InlineData("809999999", "809", "999999")]
+        [InlineData("882000000", "882", "000000")]
+        [InlineData("882999999", "882", "999999")]
+        public void Parse_Known_NonGeographicPhoneNumber_8XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+        {
+            var parseResult = _parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+            var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+            Assert.Equal(CountryInfo.Portugal, nonGeographicPhoneNumber.Country);
+            Assert.False(nonGeographicPhoneNumber.IsFreephone);
+            Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+            Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+        }
+
+        [Theory]
+        [InlineData("800000000", "800", "000000")]
+        [InlineData("800999999", "800", "999999")]
+        public void Parse_Known_NonGeographicPhoneNumber_Freephone(string value, string NationalDestinationCode, string subscriberNumber)
+        {
+            var parseResult = _parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+            var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+            Assert.Equal(CountryInfo.Portugal, nonGeographicPhoneNumber.Country);
+            Assert.True(nonGeographicPhoneNumber.IsFreephone);
+            Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+            Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+        }
+    }
+}
