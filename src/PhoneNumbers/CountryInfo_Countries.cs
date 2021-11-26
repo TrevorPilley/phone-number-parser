20f63bf7e60acff3588deeefcfc7b9e5eeba04eb
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-11-26 10:38:41 +0000

    Update CountryInfo_Countries.cs

--- src/PhoneNumbers/CountryInfo_Countries.cs
+++ src/PhoneNumbers/CountryInfo_Countries.cs
@@ -39,7 +39,7 @@ namespace PhoneNumbers
             CallingCode = "+385",
             Iso3166Code = "HR",
             Name = "Croatia",
-            NdcLengths = new ReadOnlyCollection<int>(new[] { 3, 2 }),
+            NdcLengths = new ReadOnlyCollection<int>(new[] { 2, 1 }),
             NsnLengths = new ReadOnlyCollection<int>(new[] { 8, 9 }),
             RequireNdcForLocalDialling = false,
             TrunkPrefix = "0",
