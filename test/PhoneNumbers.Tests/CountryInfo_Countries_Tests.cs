6ce37572ad5b48535af0de18880f6704212b6a8b
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-12-13 22:27:53 +0000

    Update CountryInfo_Countries_Tests.cs

--- test/PhoneNumbers.Tests/CountryInfo_Countries_Tests.cs
+++ test/PhoneNumbers.Tests/CountryInfo_Countries_Tests.cs
@@ -37,7 +37,7 @@ namespace PhoneNumbers.Tests
             Assert.Equal("Belgium", countryInfo.Name);
             Assert.Equal(new[] { 3, 2, 1 }, countryInfo.NdcLengths);
             Assert.Equal(new[] { 8, 9 }, countryInfo.NsnLengths);
-            Assert.False(countryInfo.RequireNdcForLocalDialling);
+            Assert.True(countryInfo.RequireNdcForLocalDialling);
             Assert.False(countryInfo.SharesCallingCode);
             Assert.Equal("0", countryInfo.TrunkPrefix);
         }
@@ -56,7 +56,7 @@ namespace PhoneNumbers.Tests
             Assert.Equal("Bulgaria", countryInfo.Name);
             Assert.Equal(new[] { 5, 4, 3, 2, 1 }, countryInfo.NdcLengths);
             Assert.Equal(new[] { 8, 9 }, countryInfo.NsnLengths);
-            Assert.True(countryInfo.RequireNdcForLocalDialling);
+            Assert.False(countryInfo.RequireNdcForLocalDialling);
             Assert.False(countryInfo.SharesCallingCode);
             Assert.Equal("0", countryInfo.TrunkPrefix);
         }
