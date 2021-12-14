e463b12dc31b146e5d006ae7e61ee0dc724a793e
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-12-14 14:14:45 +0000

    Update CountryInfo_Countries_Tests.cs

--- test/PhoneNumbers.Tests/CountryInfo_Countries_Tests.cs
+++ test/PhoneNumbers.Tests/CountryInfo_Countries_Tests.cs
@@ -93,7 +93,7 @@ namespace PhoneNumbers.Tests
             Assert.Equal("CZ", countryInfo.Iso3166Code);
             Assert.Equal("Czech Republic", countryInfo.Name);
             Assert.Equal(new[] { 3, 2, 1 }, countryInfo.NdcLengths);
-            Assert.Equal(new[] { 6, 8 }, countryInfo.NsnLengths);
+            Assert.Equal(new[] { 6, 8, 9 }, countryInfo.NsnLengths);
             Assert.True(countryInfo.RequireNdcForLocalDialling);
             Assert.False(countryInfo.SharesCallingCode);
             Assert.Null(countryInfo.TrunkPrefix);
