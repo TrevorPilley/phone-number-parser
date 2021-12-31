6d2f725c4c90eb241eb60daa19d3f43c4d75e1b6
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-12-31 14:09:32 +0000

    Create DefaultPhoneNumberParserTests_RO_MobilePhoneNumber.cs

---
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_RO_MobilePhoneNumber.cs
@@ -0,0 +1,65 @@
+using PhoneNumbers.Parsers;
+using Xunit;
+
+namespace PhoneNumbers.Tests.Parsers
+{
+    /// <summary>
+    /// Contains unit tests for the <see cref="DefaultPhoneNumberParser"/> class for Romania <see cref="PhoneNumber"/>s.
+    /// </summary>
+    public class DefaultPhoneNumberParserTests_RO_MobilePhoneNumber
+    {
+        private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.Romania);
+
+        [Theory]
+        [InlineData("0610000000", "61", "0000000")]
+        [InlineData("0619999999", "61", "9999999")]
+        [InlineData("0690000000", "69", "0000000")]
+        [InlineData("0699999999", "69", "9999999")]
+        [InlineData("0710000000", "71", "0000000")]
+        [InlineData("0719999999", "71", "9999999")]
+        [InlineData("0790000000", "79", "0000000")]
+        [InlineData("0799999999", "79", "9999999")]
+        public void Parse_Known_MobilePhoneNumber(string value, string NationalDestinationCode, string subscriberNumber)
+        {
+            var parseResult = s_parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+            var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+            Assert.Equal(CountryInfo.Romania, mobilePhoneNumber.Country);
+            Assert.False(mobilePhoneNumber.IsDataOnly);
+            Assert.False(mobilePhoneNumber.IsPager);
+            Assert.False(mobilePhoneNumber.IsVirtual);
+            Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+            Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+        }
+
+        [Theory]
+        [InlineData("0600000000", "60", "0000000")]
+        [InlineData("0609999999", "60", "9999999")]
+        [InlineData("0700000000", "70", "0000000")]
+        [InlineData("0709999999", "70", "9999999")]
+        public void Parse_Known_MobilePhoneNumber_Virtual(string value, string NationalDestinationCode, string subscriberNumber)
+        {
+            var parseResult = s_parser.Parse(value);
+            parseResult.ThrowIfFailure();
+
+            var phoneNumber = parseResult.PhoneNumber;
+
+            Assert.NotNull(phoneNumber);
+            Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+            var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+            Assert.Equal(CountryInfo.Romania, mobilePhoneNumber.Country);
+            Assert.False(mobilePhoneNumber.IsDataOnly);
+            Assert.False(mobilePhoneNumber.IsPager);
+            Assert.True(mobilePhoneNumber.IsVirtual);
+            Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+            Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+        }
+    }
+}
