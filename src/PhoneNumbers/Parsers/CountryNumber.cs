fc06516d2d3ea0dd4a6bb09a019e59242d512e80
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-01-05 17:22:56 +0000

    enable ImplicitUsings

--- src/PhoneNumbers/Parsers/CountryNumber.cs
+++ src/PhoneNumbers/Parsers/CountryNumber.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 namespace PhoneNumbers.Parsers
 {
     /// <summary>
--- test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests.cs
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.ObjectModel;
 using PhoneNumbers.Parsers;
 using Xunit;
