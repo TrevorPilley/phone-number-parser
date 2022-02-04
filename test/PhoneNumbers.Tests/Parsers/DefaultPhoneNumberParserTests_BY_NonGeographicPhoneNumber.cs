17ffbd625637d21cfc7f1b8d58c8a37abaf9394e
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-02-04 11:47:14 +0000

    Update DefaultPhoneNumberParserTests_BY_NonGeographicPhoneNumber.cs

--- test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_BY_NonGeographicPhoneNumber.cs
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_BY_NonGeographicPhoneNumber.cs
@@ -32,14 +32,10 @@ public class DefaultPhoneNumberParserTests_BY_NonGeographicPhoneNumber
     [Theory]
     [InlineData("0800000", "800", "000")]
     [InlineData("0800999", "800", "999")]
-    [InlineData("08030000000", "803", "0000000")]
-    [InlineData("08039999999", "803", "9999999")]
     [InlineData("08050000000", "805", "0000000")]
     [InlineData("08059999999", "805", "9999999")]
     [InlineData("08100000000", "810", "0000000")]
     [InlineData("08109999999", "810", "9999999")]
-    [InlineData("082000000000", "820", "00000000")]
-    [InlineData("082099999999", "820", "99999999")]
     public void Parse_Known_NonGeographicPhoneNumber_8XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
@@ -62,6 +58,10 @@ public class DefaultPhoneNumberParserTests_BY_NonGeographicPhoneNumber
     [Theory]
     [InlineData("08010000000", "801", "0000000")]
     [InlineData("08019999999", "801", "9999999")]
+    [InlineData("08030000000", "803", "0000000")]
+    [InlineData("08039999999", "803", "9999999")]
+    [InlineData("082000000000", "820", "00000000")]
+    [InlineData("082099999999", "820", "99999999")]
     public void Parse_Known_NonGeographicPhoneNumber_Freephone(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
