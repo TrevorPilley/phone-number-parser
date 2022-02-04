f1ad219dcb212e699bae436afa7bbaca959d7dd4
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-02-04 11:43:40 +0000

    updates

--- test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_BY_MobilePhoneNumber.cs
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_BY_MobilePhoneNumber.cs
@@ -8,6 +8,14 @@ public class DefaultPhoneNumberParserTests_BY_MobilePhoneNumber
     private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.Belarus);
 
     [Theory]
+    [InlineData("0255000000", "25", "5000000")]
+    [InlineData("0257999999", "25", "7999999")]
+    [InlineData("0259000000", "25", "9000000")]
+    [InlineData("0259999999", "25", "9999999")]
+    [InlineData("0291000000", "29", "1000000")]
+    [InlineData("0293999999", "29", "3999999")]
+    [InlineData("0295000000", "29", "5000000")]
+    [InlineData("0299999999", "29", "9999999")]
     [InlineData("0330000000", "33", "0000000")]
     [InlineData("0339999999", "33", "9999999")]
     [InlineData("0440000000", "44", "0000000")]
--- test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_BY_NonGeographicPhoneNumber.cs
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_BY_NonGeographicPhoneNumber.cs
@@ -38,6 +38,8 @@ public class DefaultPhoneNumberParserTests_BY_NonGeographicPhoneNumber
     [InlineData("08059999999", "805", "9999999")]
     [InlineData("08100000000", "810", "0000000")]
     [InlineData("08109999999", "810", "9999999")]
+    [InlineData("082000000000", "820", "00000000")]
+    [InlineData("082099999999", "820", "99999999")]
     public void Parse_Known_NonGeographicPhoneNumber_8XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
