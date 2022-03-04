767453a2d414c18500d9ce9f49436e3dc643ee38
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-03-04 14:00:04 +0000

    Update ParseOptions.cs

--- src/PhoneNumbers/ParseOptions.cs
+++ src/PhoneNumbers/ParseOptions.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using PhoneNumbers.Parsers;
 
 namespace PhoneNumbers;
@@ -9,45 +8,15 @@ namespace PhoneNumbers;
 public sealed class ParseOptions
 {
     /// <summary>
-    /// Initialises a new instance of the <see cref="ParseOptions"/> class.
-    /// </summary>
-    public ParseOptions()
-       : this( x => x != null)
-    {
-    }
-
-    private ParseOptions(Func<CountryInfo, bool> filter)
-    {
-        Countries = typeof(CountryInfo)
-            .GetProperties(BindingFlags.Public | BindingFlags.Static)
-            .Where(x => x.PropertyType == typeof(CountryInfo))
-            .Select(x => x.GetValue(null))
-            .Cast<CountryInfo>()
-            .Where(filter)
-            .OrderBy(x => x.SharesCallingCode)
-            .ToList();
-    }
-
-    /// <summary>
     /// Gets the default parse options.
     /// </summary>
     public static ParseOptions Default { get; } = new();
 
     /// <summary>
-    /// Gets the parse options, limited to countries in Asia.
-    /// </summary>
-    public static ParseOptions Asia { get; } = new(x => x.Continent == CountryInfo.Asia);
-
-    /// <summary>
-    /// Gets the parse options, limited to countries in Europe.
-    /// </summary>
-    public static ParseOptions Europe { get; } = new(x => x.Continent == CountryInfo.Europe);
-
-    /// <summary>
     /// Gets the supported <see cref="CountryInfo"/>s.
     /// </summary>
     /// <remarks>By default, this will contain all <see cref="CountryInfo"/> static properties.</remarks>
-    public ICollection<CountryInfo> Countries { get; }
+    public ICollection<CountryInfo> Countries { get; } = CountryInfo.GetCountries(x => x != null);
 
     /// <summary>
     /// Gets the <see cref="PhoneNumberParserFactory"/>.
