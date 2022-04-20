932bcc26aa54a8adc79784b519589d381e530b4c
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-04-20 10:34:29 +0100

    Correct Greece numbers

--- test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_GR_NonGeographicPhoneNumber.cs
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_GR_NonGeographicPhoneNumber.cs
@@ -8,6 +8,28 @@ public class DefaultPhoneNumberParserTests_GR_NonGeographicPhoneNumber
     private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.Greece);
 
     [Theory]
+    [InlineData("5000000000", "50", "00000000")]
+    [InlineData("5099999999", "50", "99999999")]
+    public void Parse_Known_NonGeographicPhoneNumber_5X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+        var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Greece, nonGeographicPhoneNumber.Country);
+        Assert.False(nonGeographicPhoneNumber.IsFreephone);
+        Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
+        Assert.False(nonGeographicPhoneNumber.IsSharedCost);
+        Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
     [InlineData("8060000000", "806", "0000000")]
     [InlineData("8069999999", "806", "9999999")]
     [InlineData("8120000000", "812", "0000000")]
