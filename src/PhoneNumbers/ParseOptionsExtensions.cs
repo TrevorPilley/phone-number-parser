702e00846d137e51bcd2dc27ebb279a6fc4d8c75
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-05-22 09:36:54 +0100

    Update ParseOptionsExtensions.cs

--- src/PhoneNumbers/ParseOptionsExtensions.cs
+++ src/PhoneNumbers/ParseOptionsExtensions.cs
@@ -10,30 +10,23 @@ public static class ParseOptionsExtensions
     /// </summary>
     /// <param name="parseOptions">The <see cref="ParseOptions"/> instance to update.</param>
     public static void AllowAsianCountries(this ParseOptions parseOptions)
-    {
-        if (parseOptions is null)
-        {
-            throw new ArgumentNullException(nameof(parseOptions));
-        }
-
-        foreach (var countryInfo in CountryInfo.GetCountries(x => x.Continent == CountryInfo.Asia))
-        {
-            parseOptions.Countries.Add(countryInfo);
-        }
-    }
+        => AllowCountries(parseOptions, CountryInfo.Asia);
 
     /// <summary>
     /// Allows the <see cref="ParseOptions"/> instance to include countries in Europe.
     /// </summary>
     /// <param name="parseOptions">The <see cref="ParseOptions"/> instance to update.</param>
     public static void AllowEuropeanCountries(this ParseOptions parseOptions)
+        => AllowCountries(parseOptions, CountryInfo.Europe);
+    
+    private static void AllowCountries(ParseOptions parseOptions, string continent)
     {
         if (parseOptions is null)
         {
             throw new ArgumentNullException(nameof(parseOptions));
         }
 
-        foreach (var countryInfo in CountryInfo.GetCountries(x => x.Continent == CountryInfo.Europe))
+        foreach (var countryInfo in CountryInfo.GetCountries(x => x.Continent == continent))
         {
             parseOptions.Countries.Add(countryInfo);
         }
