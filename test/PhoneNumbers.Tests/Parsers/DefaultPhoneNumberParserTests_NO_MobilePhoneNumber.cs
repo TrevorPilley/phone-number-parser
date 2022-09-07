a2eeb002f719fe007812d47f2a376a9cdb2cfe96
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-09-07 21:45:22 +0100

    tests

---
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_NO_MobilePhoneNumber.cs
@@ -0,0 +1,33 @@
+namespace PhoneNumbers.Tests.Parsers;
+
+/// <summary>
+/// Contains unit tests for the <see cref="DefaultPhoneNumberParser"/> class for Norway <see cref="PhoneNumber"/>s.
+/// </summary>
+public class DefaultPhoneNumberParserTests_NO_MobilePhoneNumber
+{
+    private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.Norway);
+
+    [Theory]
+    [InlineData("40000000", "40000000")]
+    [InlineData("49999999", "49999999")]
+    [InlineData("90000000", "90000000")]
+    [InlineData("99999999", "99999999")]
+    public void Parse_Known_MobilePhoneNumber(string value, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Norway, mobilePhoneNumber.Country);
+        Assert.False(mobilePhoneNumber.IsDataOnly);
+        Assert.False(mobilePhoneNumber.IsPager);
+        Assert.False(mobilePhoneNumber.IsVirtual);
+        Assert.Null(mobilePhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+    }
+}
