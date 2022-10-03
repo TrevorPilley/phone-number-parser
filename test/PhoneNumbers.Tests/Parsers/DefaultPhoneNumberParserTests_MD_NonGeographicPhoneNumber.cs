aa406f3ad80c22dcc8ea818c794bc825d7aeaf64
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-10-03 09:12:37 +0100

    updates

--- test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_MD_NonGeographicPhoneNumber.cs
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_MD_NonGeographicPhoneNumber.cs
@@ -8,6 +8,31 @@ public class DefaultPhoneNumberParserTests_MD_NonGeographicPhoneNumber
     private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.Moldova);
 
     [Theory]
+    [InlineData("030000000", "30", "000000")]
+    [InlineData("030999999", "30", "999999")]
+    [InlineData("038000000", "38", "000000")]
+    [InlineData("038999999", "38", "999999")]
+    public void Parse_Known_NonGeographicPhoneNumber_3X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+        var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Moldova, nonGeographicPhoneNumber.Country);
+        Assert.False(nonGeographicPhoneNumber.IsFreephone);
+        Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
+        Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
+        Assert.False(nonGeographicPhoneNumber.IsSharedCost);
+        Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
     [InlineData("080300000", "803", "00000")]
     [InlineData("080399999", "803", "99999")]
     [InlineData("081400000", "814", "00000")]
@@ -61,9 +86,9 @@ public class DefaultPhoneNumberParserTests_MD_NonGeographicPhoneNumber
     [InlineData("090000000", "900", "00000")]
     [InlineData("090099999", "900", "99999")]
     [InlineData("090500000", "905", "00000")]
-    [InlineData("090500000", "905", "00000")]
     [InlineData("090599999", "905", "99999")]
-    [InlineData("090599999", "905", "99999")]
+    [InlineData("090600000", "906", "00000")]
+    [InlineData("090699999", "906", "99999")]
     public void Parse_Known_NonGeographicPhoneNumber_PremiumRate(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
