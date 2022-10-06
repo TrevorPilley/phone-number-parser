a214d63e826b42fa589ed755a9e5b9b0e5b2d6b3
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-10-06 13:36:31 +0100

    Split CountryInfo by continent
    This should make it easier to manage the files as more countries are added.

--- src/PhoneNumbers/CountryInfo_Europe.cs
+++ src/PhoneNumbers/CountryInfo_Europe.cs
@@ -201,19 +201,6 @@ public partial class CountryInfo
     };
 
     /// <summary>
-    /// Gets the <see cref="CountryInfo"/> for Hong Kong.
-    /// </summary>
-    public static CountryInfo HongKong { get; } = new()
-    {
-        CallingCode = "+852",
-        Continent = Asia,
-        InternationalCallPrefix = "001",
-        Iso3166Code = "HK",
-        Name = "Hong Kong",
-        NsnLengths = new ReadOnlyCollection<int>(new[] { 8, 9, 12 }),
-    };
-
-    /// <summary>
     /// Gets the <see cref="CountryInfo"/> for Hungary.
     /// </summary>
     public static CountryInfo Hungary { get; } = new()
@@ -303,18 +290,6 @@ public partial class CountryInfo
         NsnLengths = new ReadOnlyCollection<int>(new[] { 8, 9 }),
         RequireNdcForLocalGeographicDialling = false,
         TrunkPrefix = "0",
-    };
-
-    /// <summary>
-    /// Gets the <see cref="CountryInfo"/> for Macau.
-    /// </summary>
-    public static CountryInfo Macau { get; } = new()
-    {
-        CallingCode = "+853",
-        Continent = Asia,
-        Iso3166Code = "MO",
-        Name = "Macau",
-        NsnLengths = new ReadOnlyCollection<int>(new[] { 8 }),
     };
 
     /// <summary>
@@ -434,19 +409,6 @@ public partial class CountryInfo
         NsnLengths = new ReadOnlyCollection<int>(new[] { 8, 9, 10, 11, 12 }),
         RequireNdcForLocalGeographicDialling = false,
         TrunkPrefix = "0",
-    };
-
-    /// <summary>
-    /// Gets the <see cref="CountryInfo"/> for Singapore.
-    /// </summary>
-    public static CountryInfo Singapore { get; } = new()
-    {
-        CallingCode = "+65",
-        Continent = Asia,
-        InternationalCallPrefix = "001",
-        Iso3166Code = "SG",
-        Name = "Singapore",
-        NsnLengths = new ReadOnlyCollection<int>(new[] { 8, 10, 11 }),
     };
 
     /// <summary>
---
+++ test/PhoneNumbers.Tests/CountryInfo_Asia_Tests.cs
@@ -0,0 +1,64 @@
+namespace PhoneNumbers.Tests;
+
+public class CountryInfo_Asia_Tests
+{
+    [Fact]
+    public void CountryInfo_HongKong()
+    {
+        Assert.Same(CountryInfo.HongKong, CountryInfo.HongKong);
+
+        var countryInfo = CountryInfo.HongKong;
+
+        Assert.Equal("+852", countryInfo.CallingCode);
+        Assert.Equal(CountryInfo.Asia, countryInfo.Continent);
+        Assert.False(countryInfo.HasNationalDestinationCodes);
+        Assert.Equal("001", countryInfo.InternationalCallPrefix);
+        Assert.Equal("HK", countryInfo.Iso3166Code);
+        Assert.Equal("Hong Kong", countryInfo.Name);
+        Assert.Empty(countryInfo.NdcLengths);
+        Assert.Equal(new[] { 8, 9, 12 }, countryInfo.NsnLengths);
+        Assert.True(countryInfo.RequireNdcForLocalGeographicDialling);
+        Assert.False(countryInfo.SharesCallingCode);
+        Assert.Null(countryInfo.TrunkPrefix);
+    }
+
+    [Fact]
+    public void CountryInfo_Macau()
+    {
+        Assert.Same(CountryInfo.Macau, CountryInfo.Macau);
+
+        var countryInfo = CountryInfo.Macau;
+
+        Assert.Equal("+853", countryInfo.CallingCode);
+        Assert.Equal(CountryInfo.Asia, countryInfo.Continent);
+        Assert.False(countryInfo.HasNationalDestinationCodes);
+        Assert.Equal("00", countryInfo.InternationalCallPrefix);
+        Assert.Equal("MO", countryInfo.Iso3166Code);
+        Assert.Equal("Macau", countryInfo.Name);
+        Assert.Empty(countryInfo.NdcLengths);
+        Assert.Equal(new[] { 8 }, countryInfo.NsnLengths);
+        Assert.True(countryInfo.RequireNdcForLocalGeographicDialling);
+        Assert.False(countryInfo.SharesCallingCode);
+        Assert.Null(countryInfo.TrunkPrefix);
+    }
+
+    [Fact]
+    public void CountryInfo_Singapore()
+    {
+        Assert.Same(CountryInfo.Singapore, CountryInfo.Singapore);
+
+        var countryInfo = CountryInfo.Singapore;
+
+        Assert.Equal("+65", countryInfo.CallingCode);
+        Assert.Equal(CountryInfo.Asia, countryInfo.Continent);
+        Assert.False(countryInfo.HasNationalDestinationCodes);
+        Assert.Equal("001", countryInfo.InternationalCallPrefix);
+        Assert.Equal("SG", countryInfo.Iso3166Code);
+        Assert.Equal("Singapore", countryInfo.Name);
+        Assert.Empty(countryInfo.NdcLengths);
+        Assert.Equal(new[] { 8, 10, 11 }, countryInfo.NsnLengths);
+        Assert.True(countryInfo.RequireNdcForLocalGeographicDialling);
+        Assert.False(countryInfo.SharesCallingCode);
+        Assert.Null(countryInfo.TrunkPrefix);
+    }
+}
