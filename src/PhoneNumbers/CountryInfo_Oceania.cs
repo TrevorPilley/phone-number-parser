3c33147f85fbec05909e7fe4f7605210641b2054
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2022-12-13 10:15:25 +0000

    Initial commit

--- src/PhoneNumbers/CountryInfo_Oceania.cs
+++ src/PhoneNumbers/CountryInfo_Oceania.cs
@@ -4,4 +4,18 @@ namespace PhoneNumbers;
 
 public partial class CountryInfo
 {
+    /// <summary>
+    /// Gets the <see cref="CountryInfo"/> for Australia.
+    /// </summary>
+    public static CountryInfo Australia { get; } = new()
+    {
+        CallingCode = "+61",
+        Continent = Oceania,
+        Iso3166Code = "AU",
+        Name = "Australia",
+        NdcLengths = new ReadOnlyCollection<int>(new[] { 4, 3, 2, 1 }),
+        NsnLengths = new ReadOnlyCollection<int>(new[] { 9 }),
+        RequireNdcForLocalGeographicDialling = false,
+        TrunkPrefix = "0",
+    };
 }
--- test/PhoneNumbers.Tests/CountryInfo_Oceania_Tests.cs
+++ test/PhoneNumbers.Tests/CountryInfo_Oceania_Tests.cs
@@ -2,4 +2,23 @@ namespace PhoneNumbers.Tests;
 
 public class CountryInfo_Oceania_Tests
 {
+    [Fact]
+    public void CountryInfo_Australia()
+    {
+        Assert.Same(CountryInfo.Australia, CountryInfo.Australia);
+
+        var countryInfo = CountryInfo.Australia;
+
+        Assert.Equal("+61", countryInfo.CallingCode);
+        Assert.Equal(CountryInfo.Oceania, countryInfo.Continent);
+        Assert.True(countryInfo.HasNationalDestinationCodes);
+        Assert.Equal("00", countryInfo.InternationalCallPrefix);
+        Assert.Equal("AU", countryInfo.Iso3166Code);
+        Assert.Equal("Australia", countryInfo.Name);
+        Assert.Equal(new[] { 4, 3, 2, 1 }, countryInfo.NdcLengths);
+        Assert.Equal(new[] { 9 }, countryInfo.NsnLengths);
+        Assert.False(countryInfo.RequireNdcForLocalGeographicDialling);
+        Assert.False(countryInfo.SharesCallingCode);
+        Assert.Equal("0", countryInfo.TrunkPrefix);
+    }
 }
