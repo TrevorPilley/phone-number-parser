b94b7132e04a756696920aa52f1f5cce7da4ec91
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2023-01-06 14:10:55 +0000

    corrections

--- test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_AU_MobilePhoneNumber.cs
+++ test/PhoneNumbers.Tests/Parsers/DefaultPhoneNumberParserTests_AU_MobilePhoneNumber.cs
@@ -30,6 +30,28 @@ public class DefaultPhoneNumberParserTests_AU_MobilePhoneNumber
     }
 
     [Theory]
+    [InlineData("016000", "16", "000")]
+    [InlineData("0169999999", "16", "9999999")]
+    public void Parse_Known_MobilePhoneNumber_Pager(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Australia, mobilePhoneNumber.Country);
+        Assert.False(mobilePhoneNumber.IsDataOnly);
+        Assert.True(mobilePhoneNumber.IsPager);
+        Assert.False(mobilePhoneNumber.IsVirtual);
+        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
     [InlineData("0500000000", "50", "0000000")]
     [InlineData("0509999999", "50", "9999999")]
     public void Parse_Known_MobilePhoneNumber_Virtual(string value, string NationalDestinationCode, string subscriberNumber)
