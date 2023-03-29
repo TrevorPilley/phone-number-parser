d66541d5336a4ca760ebbbf82c1389523ac110b3
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2023-03-29 19:28:12 +0000

    Move common characters to constants

--- src/PhoneNumbers/Chars.cs
+++ src/PhoneNumbers/Chars.cs
@@ -2,5 +2,10 @@ namespace PhoneNumbers;
 
 internal static class Chars
 {
+    internal const char Comma = ',';
+    internal const char Hash = '#';
+	internal const char Hyphen = '-';
+	internal const char Null = '\0';
+    internal const char Pipe = '|';
     internal const char Plus = '+';
 }
