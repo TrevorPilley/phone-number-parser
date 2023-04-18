08a3134ddf720d233932c82e4f45a128c51c3001
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2023-04-18 11:33:25 +0100

    Tidy up

--- src/PhoneNumbers/Formatters/FormatProviders/GBPhoneNumberFormatProvider.cs
+++ src/PhoneNumbers/Formatters/FormatProviders/GBPhoneNumberFormatProvider.cs
@@ -20,11 +20,11 @@ internal sealed class GBPhoneNumberFormatProvider : ComplexPhoneNumberFormatProv
         {
             6 => phoneNumber.NationalDestinationCode!.Length switch
             {
-                #pragma warning disable S3358 // Extract this nested ternary operation into an independent statement.
+#pragma warning disable S3358 // Extract this nested ternary operation into an independent statement.
                 3 => international ? "### ######" : ndcOptional ? "(0###) ######" : "0### ######",
                 4 => international ? "#### ######" : ndcOptional ? "(0####) ######" : "0#### ######",
                 5 => international ? "##### ######" : ndcOptional ? "(0#####) ######" : "0##### ######",
-                #pragma warning restore S3358 // Extract this nested ternary operation into an independent statement.
+#pragma warning restore S3358 // Extract this nested ternary operation into an independent statement.
                 _ => base.ProvideFormat(phoneNumber, international),
             },
             _ => base.ProvideFormat(phoneNumber, international),
