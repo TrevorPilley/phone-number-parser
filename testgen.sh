5a216aa297ab08e88dc25e0b0cd8ddb06d833273
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2023-05-08 15:58:11 +0000

    Add the test gen utility

---
+++ testgen.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dotnet run --project utilities/phone-number-utilities-testgenerator/src/PhoneNumbers.Utilities.TestGenerator/PhoneNumbers.Utilities.TestGenerator.csproj /dataFilePath=/workspaces/phone-number-parser/src/PhoneNumbers/DataFiles
