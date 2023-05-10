a3c52ff3e063f65ba9181ffaa010d7003c7673fd
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2023-05-10 14:56:42 +0000

    Update the submodule and then clean

--- testgen.sh
+++ testgen.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+git submodule update --recursive --remote
+
 dotnet run --project utilities/phone-number-utilities-testgenerator/src/PhoneNumbers.Utilities.TestGenerator/PhoneNumbers.Utilities.TestGenerator.csproj /dataFilePath=/workspaces/phone-number-parser/src/PhoneNumbers/DataFiles
+
+dotnet clean --verbosity minimal
