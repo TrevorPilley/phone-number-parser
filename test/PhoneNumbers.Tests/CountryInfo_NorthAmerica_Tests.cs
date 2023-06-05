817ffcb8e741d220b11706f1ad7572aa7f5108ad
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2023-06-05 11:37:12 +0100

    Closes #496
    Add support for Cayman Islands numbers

--- test/PhoneNumbers.Tests/CountryInfo_NorthAmerica_Tests.cs
+++ test/PhoneNumbers.Tests/CountryInfo_NorthAmerica_Tests.cs
@@ -145,6 +145,26 @@ public class CountryInfo_NorthAmerica_Tests
     }
 
     [Fact]
+    public void CountryInfo_CaymanIslands()
+    {
+        Assert.Same(CountryInfo.CaymanIslands, CountryInfo.CaymanIslands);
+
+        var countryInfo = CountryInfo.CaymanIslands;
+
+        Assert.True(countryInfo.AllowsLocalGeographicDialling);
+        Assert.Equal(CountryInfo.NanpCallingCode, countryInfo.CallingCode);
+        Assert.Equal(CountryInfo.NorthAmerica, countryInfo.Continent);
+        Assert.IsType<NanpPhoneNumberFormatProvider>(countryInfo.FormatProvider);
+        Assert.False(countryInfo.IsEuropeanUnionMember);
+        Assert.Equal("KY", countryInfo.Iso3166Code);
+        Assert.Equal("Cayman Islands", countryInfo.Name);
+        Assert.Equal(new[] { 3 }, countryInfo.NdcLengths);
+        Assert.Equal(new[] { 10 }, countryInfo.NsnLengths);
+        Assert.True(countryInfo.SharesCallingCode);
+        Assert.Null(countryInfo.TrunkPrefix);
+    }
+
+    [Fact]
     public void CountryInfo_PuertoRico()
     {
         Assert.Same(CountryInfo.PuertoRico, CountryInfo.PuertoRico);
