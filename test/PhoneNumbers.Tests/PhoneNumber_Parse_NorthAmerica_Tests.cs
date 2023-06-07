b882aa5dd0a1cfa46888be4d1dbeaed7e20169da
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2023-06-07 15:06:28 +0000

    Closes #521
    Add support for Northern Mariana Island phone numbers

--- test/PhoneNumbers.Tests/PhoneNumber_Parse_NorthAmerica_Tests.cs
+++ test/PhoneNumbers.Tests/PhoneNumber_Parse_NorthAmerica_Tests.cs
@@ -91,6 +91,14 @@ public class PhoneNumber_Parse_NorthAmerica_Tests
     }
 
     [Fact]
+    public void Parse_Value_For_NorthernMarianaIsland_CallingCode()
+    {
+        var phoneNumber = PhoneNumber.Parse("+16702202200");
+        Assert.NotNull(phoneNumber);
+        Assert.Equal(CountryInfo.NorthernMarianaIsland, phoneNumber.Country);
+    }
+
+    [Fact]
     public void Parse_Value_For_PuertoRico_CallingCode()
     {
         var phoneNumber = PhoneNumber.Parse("+17877222977");
