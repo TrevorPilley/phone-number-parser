7d97e09e837b405053cd12de2b2e556c53b92475
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2023-11-20 11:47:44 +0000

    Remove obsolete constructor

--- src/PhoneNumbers/ParseException.cs
+++ src/PhoneNumbers/ParseException.cs
@@ -5,7 +5,6 @@ namespace PhoneNumbers;
 /// <summary>
 /// An exception thrown when there is an issue parsing a phone number.
 /// </summary>
-[Serializable]
 [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public class ParseException : Exception
 {
@@ -32,16 +31,6 @@ public class ParseException : Exception
     /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public ParseException(string? message, Exception? innerException)
         : base(message, innerException)
-    {
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ParseException"/> class.
-    /// </summary>
-    /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
-    /// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
-    protected ParseException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
     {
     }
 }
