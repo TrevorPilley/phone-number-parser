071ea324f1965518a202b9bf491bfb84764c61dd
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-04-15 08:57:55 +0000

    updates

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_TZ_NonGeographicPhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_TZ_NonGeographicPhoneNumber.cs
@@ -56,18 +56,35 @@ public class DefaultPhoneNumberParserTests_TZ_NonGeographicPhoneNumber
     }
 
     [Theory]
-    [InlineData("0800110000", "800", "110000")]
-    [InlineData("0800129999", "800", "129999")]
-    [InlineData("0800710000", "800", "710000")]
-    [InlineData("0800719999", "800", "719999")]
-    [InlineData("0800750000", "800", "750000")]
-    [InlineData("0800759999", "800", "759999")]
-    [InlineData("0800780000", "800", "780000")]
-    [InlineData("0800789999", "800", "789999")]
     [InlineData("0808000000", "808", "000000")]
-    [InlineData("0808009999", "808", "009999")]
-    [InlineData("0808110000", "808", "110000")]
-    [InlineData("0808119999", "808", "119999")]
+    [InlineData("0808999999", "808", "999999")]
+    [InlineData("0860000000", "860", "000000")]
+    [InlineData("0860999999", "860", "999999")]
+    [InlineData("0861000000", "861", "000000")]
+    [InlineData("0861999999", "861", "999999")]
+    public void Parse_Known_NonGeographicPhoneNumber_8XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+        var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Tanzania, nonGeographicPhoneNumber.Country);
+        Assert.False(nonGeographicPhoneNumber.IsFreephone);
+        Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
+        Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
+        Assert.False(nonGeographicPhoneNumber.IsSharedCost);
+        Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
+    [InlineData("0800000000", "800", "000000")]
+    [InlineData("0800999999", "800", "999999")]
     public void Parse_Known_NonGeographicPhoneNumber_Freephone(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
@@ -89,8 +106,8 @@ public class DefaultPhoneNumberParserTests_TZ_NonGeographicPhoneNumber
     }
 
     [Theory]
-    [InlineData("0300010000000", "30001", "0000000")]
-    [InlineData("0300019999999", "30001", "9999999")]
+    [InlineData("0300000000000", "30000", "0000000")]
+    [InlineData("0300009999999", "30000", "9999999")]
     [InlineData("0300050000000", "30005", "0000000")]
     [InlineData("0300059999999", "30005", "9999999")]
     public void Parse_Known_NonGeographicPhoneNumber_MachineToMachine(string value, string NationalDestinationCode, string subscriberNumber)
@@ -116,16 +133,8 @@ public class DefaultPhoneNumberParserTests_TZ_NonGeographicPhoneNumber
     [Theory]
     [InlineData("0900010000", "900", "010000")]
     [InlineData("0900019999", "900", "019999")]
-    [InlineData("0901000000", "901", "000000")]
-    [InlineData("0901009999", "901", "009999")]
-    [InlineData("0901120000", "901", "120000")]
-    [InlineData("0901129999", "901", "129999")]
-    [InlineData("0901220000", "901", "220000")]
-    [InlineData("0901229999", "901", "229999")]
-    [InlineData("0901650000", "901", "650000")]
-    [InlineData("0901659999", "901", "659999")]
-    [InlineData("0901760000", "901", "760000")]
-    [InlineData("0901769999", "901", "769999")]
+    [InlineData("0902010000", "902", "010000")]
+    [InlineData("0902019999", "902", "019999")]
     public void Parse_Known_NonGeographicPhoneNumber_PremiumRate(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
@@ -142,6 +151,29 @@ public class DefaultPhoneNumberParserTests_TZ_NonGeographicPhoneNumber
         Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
         Assert.True(nonGeographicPhoneNumber.IsPremiumRate);
         Assert.False(nonGeographicPhoneNumber.IsSharedCost);
+        Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
+    [InlineData("0840000000", "840", "000000")]
+    [InlineData("0840999999", "840", "999999")]
+    public void Parse_Known_NonGeographicPhoneNumber_SharedCost(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+        var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Tanzania, nonGeographicPhoneNumber.Country);
+        Assert.False(nonGeographicPhoneNumber.IsFreephone);
+        Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
+        Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
+        Assert.True(nonGeographicPhoneNumber.IsSharedCost);
         Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
         Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
     }
