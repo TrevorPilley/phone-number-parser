4898f5b16830217b4f770fea1d65ade0a551efc1
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-04-16 20:23:20 +0000

    Closes #639
    Update South Africa numbers

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_ZA_MobilePhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_ZA_MobilePhoneNumber.cs
@@ -8,18 +8,22 @@ public class DefaultPhoneNumberParserTests_ZA_MobilePhoneNumber
     private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.SouthAfrica);
 
     [Theory]
+    [InlineData("0500000000", "50", "0000000")]
+    [InlineData("0509999999", "50", "9999999")]
+    [InlineData("0520000000", "52", "0000000")]
+    [InlineData("0529999999", "52", "9999999")]
+    [InlineData("0550000000", "55", "0000000")]
+    [InlineData("0559999999", "55", "9999999")]
+    [InlineData("0590000000", "59", "0000000")]
+    [InlineData("0599999999", "59", "9999999")]
     [InlineData("0600000000", "60", "0000000")]
     [InlineData("0609999999", "60", "9999999")]
-    [InlineData("0740000000", "74", "0000000")]
-    [InlineData("0749999999", "74", "9999999")]
-    [InlineData("0760000000", "76", "0000000")]
-    [InlineData("0769999999", "76", "9999999")]
     [InlineData("0790000000", "79", "0000000")]
     [InlineData("0799999999", "79", "9999999")]
     [InlineData("0810000000", "81", "0000000")]
     [InlineData("0819999999", "81", "9999999")]
-    [InlineData("0840000000", "84", "0000000")]
-    [InlineData("0849999999", "84", "9999999")]
+    [InlineData("0850000000", "85", "0000000")]
+    [InlineData("0859999999", "85", "9999999")]
     public void Parse_Known_MobilePhoneNumber(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
