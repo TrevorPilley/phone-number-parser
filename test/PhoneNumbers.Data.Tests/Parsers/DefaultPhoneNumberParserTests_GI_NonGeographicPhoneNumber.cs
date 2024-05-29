477501a888ae4190f2dfe9d97f6018ad6fce9728
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-05-29 21:29:55 +0000

    Closes #667
    Update Gibraltar Numbers

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_GI_NonGeographicPhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_GI_NonGeographicPhoneNumber.cs
@@ -8,29 +8,6 @@ public class DefaultPhoneNumberParserTests_GI_NonGeographicPhoneNumber
     private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.Gibraltar);
 
     [Theory]
-    [InlineData("8700", "87", "00")]
-    [InlineData("8799", "87", "99")]
-    public void Parse_Known_NonGeographicPhoneNumber_8X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
-    {
-        var parseResult = s_parser.Parse(value);
-        parseResult.ThrowIfFailure();
-
-        var phoneNumber = parseResult.PhoneNumber;
-
-        Assert.NotNull(phoneNumber);
-        Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
-
-        var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
-        Assert.Equal(CountryInfo.Gibraltar, nonGeographicPhoneNumber.Country);
-        Assert.False(nonGeographicPhoneNumber.IsFreephone);
-        Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
-        Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
-        Assert.False(nonGeographicPhoneNumber.IsSharedCost);
-        Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
-        Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
-    }
-
-    [Theory]
     [InlineData("8000", "80", "00")]
     [InlineData("8099", "80", "99")]
     public void Parse_Known_NonGeographicPhoneNumber_Freephone(string value, string NationalDestinationCode, string subscriberNumber)
@@ -54,10 +31,10 @@ public class DefaultPhoneNumberParserTests_GI_NonGeographicPhoneNumber
     }
 
     [Theory]
-    [InlineData("8100", "81", "00")]
-    [InlineData("8199", "81", "99")]
     [InlineData("8600", "86", "00")]
     [InlineData("8699", "86", "99")]
+    [InlineData("8800", "88", "00")]
+    [InlineData("8899", "88", "99")]
     [InlineData("8900", "89", "00")]
     [InlineData("8999", "89", "99")]
     public void Parse_Known_NonGeographicPhoneNumber_PremiumRate(string value, string NationalDestinationCode, string subscriberNumber)
@@ -76,6 +53,29 @@ public class DefaultPhoneNumberParserTests_GI_NonGeographicPhoneNumber
         Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
         Assert.True(nonGeographicPhoneNumber.IsPremiumRate);
         Assert.False(nonGeographicPhoneNumber.IsSharedCost);
+        Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
+    [InlineData("8700", "87", "00")]
+    [InlineData("8799", "87", "99")]
+    public void Parse_Known_NonGeographicPhoneNumber_SharedCost(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+        var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Gibraltar, nonGeographicPhoneNumber.Country);
+        Assert.False(nonGeographicPhoneNumber.IsFreephone);
+        Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
+        Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
+        Assert.True(nonGeographicPhoneNumber.IsSharedCost);
         Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
         Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
     }
