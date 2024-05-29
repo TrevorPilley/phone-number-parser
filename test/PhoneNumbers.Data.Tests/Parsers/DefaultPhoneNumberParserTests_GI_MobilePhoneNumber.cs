7642c3f4c06598b87cb0da6daaf805333c41fbc6
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-05-29 21:29:55 +0000

    Closes #667
    Update Gibraltar Numbers

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_GI_MobilePhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_GI_MobilePhoneNumber.cs
@@ -8,10 +8,16 @@ public class DefaultPhoneNumberParserTests_GI_MobilePhoneNumber
     private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.Gibraltar);
 
     [Theory]
-    [InlineData("50000000", "50", "000000")]
-    [InlineData("50999999", "50", "999999")]
-    [InlineData("59000000", "59", "000000")]
-    [InlineData("59999999", "59", "999999")]
+    [InlineData("51000000", "51", "000000")]
+    [InlineData("51099999", "51", "099999")]
+    [InlineData("52500000", "52", "500000")]
+    [InlineData("52514999", "52", "514999")]
+    [InlineData("56000000", "56", "000000")]
+    [InlineData("56999999", "56", "999999")]
+    [InlineData("58000000", "58", "000000")]
+    [InlineData("58999999", "58", "999999")]
+    [InlineData("60100000", "60", "100000")]
+    [InlineData("60119999", "60", "119999")]
     public void Parse_Known_MobilePhoneNumber(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
