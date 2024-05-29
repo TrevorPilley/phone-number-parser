cb4ae57a65ba8875b8f2c189b99eeef857126372
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-05-29 20:05:13 +0000

    Closes #665
    Update Czech Republic Numbers

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_CZ_MobilePhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_CZ_MobilePhoneNumber.cs
@@ -12,10 +12,14 @@ public class DefaultPhoneNumberParserTests_CZ_MobilePhoneNumber
     [InlineData("601999999", "601", "999999")]
     [InlineData("608000000", "608", "000000")]
     [InlineData("608999999", "608", "999999")]
+    [InlineData("610000000", "610", "000000")]
+    [InlineData("610999999", "610", "999999")]
+    [InlineData("614000000", "614", "000000")]
+    [InlineData("614999999", "614", "999999")]
     [InlineData("702000000", "702", "000000")]
     [InlineData("702999999", "702", "999999")]
-    [InlineData("705000000", "705", "000000")]
-    [InlineData("705999999", "705", "999999")]
+    [InlineData("719000000", "719", "000000")]
+    [InlineData("719999999", "719", "999999")]
     [InlineData("720000000", "72", "0000000")]
     [InlineData("729999999", "72", "9999999")]
     [InlineData("730000000", "73", "0000000")]
@@ -45,8 +49,6 @@ public class DefaultPhoneNumberParserTests_CZ_MobilePhoneNumber
     [Theory]
     [InlineData("700000000", "700", "000000")]
     [InlineData("700999999", "700", "999999")]
-    [InlineData("701000000", "701", "000000")]
-    [InlineData("701999999", "701", "999999")]
     public void Parse_Known_MobilePhoneNumber_Virtual(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
