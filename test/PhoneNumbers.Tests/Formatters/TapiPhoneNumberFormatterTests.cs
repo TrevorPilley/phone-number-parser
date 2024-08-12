41c729aea15786ed4c3fbcd21930e684eb1db0f6
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-08-12 14:26:27 +0300

    Update TapiPhoneNumberFormatterTests.cs

--- test/PhoneNumbers.Tests/Formatters/TapiPhoneNumberFormatterTests.cs
+++ test/PhoneNumbers.Tests/Formatters/TapiPhoneNumberFormatterTests.cs
@@ -25,3 +25,4 @@ public class TapiPhoneNumberFormatterTests
         Assert.NotNull(TapiPhoneNumberFormatter.Instance);
         Assert.Same(TapiPhoneNumberFormatter.Instance, TapiPhoneNumberFormatter.Instance);
     }
+}
