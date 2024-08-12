a5294026993b08fcfbf59bf32f4c5fe56ac356be
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-08-12 14:16:19 +0300

    Create TapiPhoneNumberFormatter.cs

---
+++ src/PhoneNumbers/Formatters/TapiPhoneNumberFormatter.cs
@@ -0,0 +1,24 @@
+namespace PhoneNumbers.Formatters;
+
+/// <summary>
+/// A <see cref="PhoneNumberFormatter"/> which returns the Telephony Application Programming Interface (TAPI) international format.
+/// </summary>
+internal sealed class TapiPhoneNumberFormatter : PhoneNumberFormatter
+{
+    /// <summary>
+    /// Initialises a new instance of the <see cref="TapiPhoneNumberFormatter"/> class.
+    /// </summary>
+    private TapiPhoneNumberFormatter()
+        : base("TAPI")
+    {
+    }
+
+    /// <summary>
+    /// Gets the <see cref="TapiPhoneNumberFormatter"/> instance.
+    /// </summary>
+    internal static PhoneNumberFormatter Instance { get; } = new TapiPhoneNumberFormatter();
+
+    /// <inheritdoc/>
+    internal override string Format(PhoneNumber phoneNumber) =>
+        FormatInternational(phoneNumber, charBetweenCallingCodeAndNsn: Chars.Space);
+}
