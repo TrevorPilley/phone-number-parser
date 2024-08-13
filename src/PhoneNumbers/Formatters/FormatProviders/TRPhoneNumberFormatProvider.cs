0d815319516c322ed392f0fc2c665babb3ecea67
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-08-13 22:31:20 +0300

    Create TRPhoneNumberFormatProvider.cs

---
+++ src/PhoneNumbers/Formatters/FormatProviders/TRPhoneNumberFormatProvider.cs
@@ -0,0 +1,20 @@
+namespace PhoneNumbers.Formatters.FormatProviders;
+
+/// <summary>
+/// A <see cref="PhoneNumberFormatProvider"/> for Turkey numbers.
+/// </summary>
+internal sealed class TRPhoneNumberFormatProvider : ComplexPhoneNumberFormatProvider
+{
+    private TRPhoneNumberFormatProvider()
+    {
+    }
+
+    internal static PhoneNumberFormatProvider Instance { get; } = new TRPhoneNumberFormatProvider();
+
+    protected override string ProvideFormat(PhoneNumber phoneNumber, bool international) =>
+        phoneNumber.NationalSignificantNumber.Length switch
+        {
+            10 => international ? "### ### ## ##" : "(0###) ### ## ##",
+            _ => base.ProvideFormat(phoneNumber, international),
+        };
+}
