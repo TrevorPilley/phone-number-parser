6c45ab924e50096eba11735a2d6d4cadce881975
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-10-30 12:55:44 +0000

    Closes #729
    Add a new formatter for Unformatted

--- test/PhoneNumbers.Tests/PhoneNumber_ToString_Africa_Tests.cs
+++ test/PhoneNumbers.Tests/PhoneNumber_ToString_Africa_Tests.cs
@@ -6,6 +6,7 @@ public class PhoneNumber_ToString_Africa_Tests
     [InlineData("+20235344239", "E.123", "+20 2 3534 4239")]
     [InlineData("+20235344239", "N", "02 3534 4239")]
     [InlineData("+20235344239", "RFC3966", "tel:+20-2-3534-4239")]
+    [InlineData("+20235344239", "U", "0235344239")]
     public void Egypt_Numbers(string input, string format, string expected) =>
         Assert.Equal(expected, PhoneNumber.Parse(input).ToString(format));
 
@@ -16,6 +17,8 @@ public class PhoneNumber_ToString_Africa_Tests
     [InlineData("+254703042000", "N", "0703 042000")]
     [InlineData("+25420424200", "RFC3966", "tel:+254-20-424200")]
     [InlineData("+254703042000", "RFC3966", "tel:+254-703-042000")]
+    [InlineData("+25420424200", "U", "020424200")]
+    [InlineData("+254703042000", "U", "0703042000")]
     public void Kenya_Numbers(string input, string format, string expected) =>
         Assert.Equal(expected, PhoneNumber.Parse(input).ToString(format));
 
@@ -23,6 +26,7 @@ public class PhoneNumber_ToString_Africa_Tests
     [InlineData("+2342094617000", "E.123", "+234 209 461 7000")]
     [InlineData("+2342094617000", "N", "(0209) 461 7000")]
     [InlineData("+2342094617000", "RFC3966", "tel:+234-209-461-7000")]
+    [InlineData("+2342094617000", "U", "02094617000")]
     public void Nigeria_Numbers(string input, string format, string expected) =>
         Assert.Equal(expected, PhoneNumber.Parse(input).ToString(format));
 
@@ -30,6 +34,7 @@ public class PhoneNumber_ToString_Africa_Tests
     [InlineData("+27215616800", "E.123", "+27 21 561 6800")]
     [InlineData("+27215616800", "N", "021 561 6800")]
     [InlineData("+27215616800", "RFC3966", "tel:+27-21-561-6800")]
+    [InlineData("+27215616800", "U", "0215616800")]
     public void SouthAfrica_Numbers(string input, string format, string expected) =>
         Assert.Equal(expected, PhoneNumber.Parse(input).ToString(format));
 
@@ -37,6 +42,7 @@ public class PhoneNumber_ToString_Africa_Tests
     [InlineData("+255222199760", "E.123", "+255 22 219 9760")]
     [InlineData("+255222199760", "N", "022 219 9760")]
     [InlineData("+255222199760", "RFC3966", "tel:+255-22-219-9760")]
+    [InlineData("+255222199760", "U", "0222199760")]
     public void Tanzania_Numbers(string input, string format, string expected) =>
         Assert.Equal(expected, PhoneNumber.Parse(input).ToString(format));
 
@@ -49,7 +55,9 @@ public class PhoneNumber_ToString_Africa_Tests
     [InlineData("+256800100960", "N", "0800 100 960")]
     [InlineData("+256392001524", "RFC3966", "tel:+256-39-200-1524")]
     [InlineData("+256414348832", "RFC3966", "tel:+256-41-434-8832")]
-    [InlineData("+256800100960", "RFC3966", "tel:+256-800-100-960")]
+    [InlineData("+256392001524", "U", "0392001524")]
+    [InlineData("+256414348832", "U", "0414348832")]
+    [InlineData("+256800100960", "U", "0800100960")]
     public void Uganda_Numbers(string input, string format, string expected) =>
         Assert.Equal(expected, PhoneNumber.Parse(input).ToString(format));
 }
--- test/PhoneNumbers.Tests/PhoneNumber_ToString_SouthAmerica_Tests.cs
+++ test/PhoneNumbers.Tests/PhoneNumber_ToString_SouthAmerica_Tests.cs
@@ -9,6 +9,8 @@ public class PhoneNumber_ToString_SouthAmerica_Tests
     [InlineData("+5541732649808", "N", "(041) 73264-9808")]
     [InlineData("+556123122026", "RFC3966", "tel:+55-61-2312-2026")]
     [InlineData("+5541732649808", "RFC3966", "tel:+55-41-73264-9808")]
+    [InlineData("+556123122026", "U", "06123122026")]
+    [InlineData("+5541732649808", "U", "041732649808")]
     public void Brazil_Numbers(string input, string format, string expected) =>
         Assert.Equal(expected, PhoneNumber.Parse(input).ToString(format));
 
@@ -16,6 +18,7 @@ public class PhoneNumber_ToString_SouthAmerica_Tests
     [InlineData("+576013198300", "E.123", "+57 601 3198300")]
     [InlineData("+576013198300", "N", "601 3198300")]
     [InlineData("+576013198300", "RFC3966", "tel:+57-601-3198300")]
+    [InlineData("+576013198300", "U", "6013198300")]
     public void Colombia_Numbers(string input, string format, string expected) =>
         Assert.Equal(expected, PhoneNumber.Parse(input).ToString(format));
 }
