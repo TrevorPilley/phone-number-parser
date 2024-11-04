3280dddf3622418b49506d31aafe136deabc5502
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-11-04 07:26:40 +0000

    Simplify equality logic
    Since a national significant number must be unique within a numbering plan, we only need to check the country and NSN.

--- test/PhoneNumbers.Tests/MobilePhoneNumberTests.cs
+++ test/PhoneNumbers.Tests/MobilePhoneNumberTests.cs
@@ -58,13 +58,10 @@ public class MobilePhoneNumberTests
     [Fact]
     public void Equality_Same_Instance()
     {
-        var phoneNumber1 = new MobilePhoneNumber(PhoneNumberHint.None)
-        {
-            Country = CountryInfo.UnitedKingdom,
-            NationalDestinationCode = "7654",
-            NationalSignificantNumber = "7654112233",
-            SubscriberNumber = "112233",
-        };
+        var phoneNumber1 = TestHelper.CreateMobilePhoneNumber(
+            trunkPrefix: default,
+            ndc: "12345",
+            sn: "667788");
         var phoneNumber2 = phoneNumber1;
 
         Assert.Equal(phoneNumber1, phoneNumber2);
@@ -79,20 +76,15 @@ public class MobilePhoneNumberTests
     [Fact]
     public void Equality_Same_Values_With_NationalDestinationCode()
     {
-        var phoneNumber1 = new MobilePhoneNumber(PhoneNumberHint.None)
-        {
-            Country = CountryInfo.UnitedKingdom,
-            NationalDestinationCode = "7654",
-            NationalSignificantNumber = "7654112233",
-            SubscriberNumber = "112233",
-        };
-        var phoneNumber2 = new MobilePhoneNumber(PhoneNumberHint.None)
-        {
-            Country = CountryInfo.UnitedKingdom,
-            NationalDestinationCode = "7654",
-            NationalSignificantNumber = "7654112233",
-            SubscriberNumber = "112233",
-        };
+        var phoneNumber1 = TestHelper.CreateMobilePhoneNumber(
+            trunkPrefix: default,
+            ndc: "12345",
+            sn: "667788");
+
+        var phoneNumber2 = TestHelper.CreateMobilePhoneNumber(
+            trunkPrefix: default,
+            ndc: "12345",
+            sn: "667788");
 
         Assert.Equal(phoneNumber1, phoneNumber2);
         Assert.True(phoneNumber1.Equals(phoneNumber2));
@@ -104,20 +96,15 @@ public class MobilePhoneNumberTests
     [Fact]
     public void Equality_Same_Values_Without_NationalDestinationCode()
     {
-        var phoneNumber1 = new MobilePhoneNumber(PhoneNumberHint.None)
-        {
-            Country = CountryInfo.UnitedKingdom,
-            NationalDestinationCode = null,
-            NationalSignificantNumber = "112233",
-            SubscriberNumber = "112233",
-        };
-        var phoneNumber2 = new MobilePhoneNumber(PhoneNumberHint.None)
-        {
-            Country = CountryInfo.UnitedKingdom,
-            NationalDestinationCode = null,
-            NationalSignificantNumber = "112233",
-            SubscriberNumber = "112233",
-        };
+        var phoneNumber1 = TestHelper.CreateMobilePhoneNumber(
+            trunkPrefix: default,
+            ndc: default,
+            sn: "667788");
+
+        var phoneNumber2 = TestHelper.CreateMobilePhoneNumber(
+            trunkPrefix: default,
+            ndc: default,
+            sn: "667788");
 
         Assert.Equal(phoneNumber1, phoneNumber2);
         Assert.True(phoneNumber1.Equals(phoneNumber2));
@@ -129,13 +116,11 @@ public class MobilePhoneNumberTests
     [Fact]
     public void Inequality()
     {
-        var phoneNumber1 = new MobilePhoneNumber(PhoneNumberHint.None)
-        {
-            Country = CountryInfo.UnitedKingdom,
-            NationalDestinationCode = "7654",
-            NationalSignificantNumber = "7654112233",
-            SubscriberNumber = "112233",
-        };
+        var phoneNumber1 = TestHelper.CreateMobilePhoneNumber(
+            trunkPrefix: default,
+            ndc: "12345",
+            sn: "667788");
+
         var phoneNumber2 = default(MobilePhoneNumber);
 
         Assert.NotEqual(phoneNumber1, phoneNumber2);
@@ -151,14 +136,11 @@ public class MobilePhoneNumberTests
         Assert.True(phoneNumber1 != (object)phoneNumber2);
         Assert.True(phoneNumber2 != (object)phoneNumber1);
 
-        // Change area code
-        var phoneNumber3 =new MobilePhoneNumber(PhoneNumberHint.None)
-        {
-            Country = CountryInfo.UnitedKingdom,
-            NationalDestinationCode = "7655",
-            NationalSignificantNumber = "7655112233",
-            SubscriberNumber = "112233",
-        };
+        // Change national destination code
+        var phoneNumber3 = TestHelper.CreateMobilePhoneNumber(
+            trunkPrefix: default,
+            ndc: "12346",
+            sn: "667788");
 
         Assert.NotEqual(phoneNumber1, phoneNumber3);
         Assert.False(phoneNumber1.Equals(phoneNumber3));
@@ -166,17 +148,28 @@ public class MobilePhoneNumberTests
         Assert.True(phoneNumber1 != phoneNumber3);
 
         // change subscriber number
-        var phoneNumber4 = new MobilePhoneNumber(PhoneNumberHint.None)
-        {
-            Country = CountryInfo.UnitedKingdom,
-            NationalDestinationCode = "7654",
-            NationalSignificantNumber = "7654112234",
-            SubscriberNumber = "112234",
-        };
+        var phoneNumber4 = TestHelper.CreateMobilePhoneNumber(
+            trunkPrefix: default,
+            ndc: "12345",
+            sn: "667789");
 
         Assert.NotEqual(phoneNumber1, phoneNumber4);
         Assert.False(phoneNumber1.Equals(phoneNumber4));
         Assert.False(phoneNumber1 == phoneNumber4);
         Assert.True(phoneNumber1 != phoneNumber4);
+
+        // change country
+        var phoneNumber5 = new MobilePhoneNumber(PhoneNumberHint.None)
+        {
+            Country = CountryInfo.France,
+            NationalDestinationCode = phoneNumber1.NationalDestinationCode,
+            NationalSignificantNumber = phoneNumber1.NationalSignificantNumber,
+            SubscriberNumber = phoneNumber1.SubscriberNumber,
+        };
+
+        Assert.NotEqual(phoneNumber1, phoneNumber5);
+        Assert.False(phoneNumber1.Equals(phoneNumber5));
+        Assert.False(phoneNumber1 == phoneNumber5);
+        Assert.True(phoneNumber1 != phoneNumber5);
     }
 }
