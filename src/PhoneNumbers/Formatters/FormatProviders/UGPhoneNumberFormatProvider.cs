90eb089b282025fddd40c6baae29a0d53ded7d72
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2024-11-23 09:20:44 +0000

    Create UGPhoneNumberFormatProvider.cs

---
+++ src/PhoneNumbers/Formatters/FormatProviders/UGPhoneNumberFormatProvider.cs
@@ -0,0 +1,24 @@
+namespace PhoneNumbers.Formatters.FormatProviders;
+
+/// <summary>
+/// A <see cref="PhoneNumberFormatProvider"/> for Uganda numbers.
+/// </summary>
+internal sealed class UGPhoneNumberFormatProvider : ComplexPhoneNumberFormatProvider
+{
+    private UGPhoneNumberFormatProvider()
+    {
+    }
+
+    internal static PhoneNumberFormatProvider Instance { get; } = new UGPhoneNumberFormatProvider();
+
+    protected override string ProvideFormat(PhoneNumber phoneNumber, bool international)
+    {
+        if (phoneNumber.NationalDestinationCode![0] == 3 ||
+            phoneNumber.NationalDestinationCode![0] == 4)
+        {
+            return international ? "## ### ####" : "0## ### ####";
+        }
+
+        return base.ProvideFormat(phoneNumber, international);
+    }
+}
