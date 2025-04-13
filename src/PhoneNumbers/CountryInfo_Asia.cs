b4624d5f0633a6b3ec563f35930e3538b2322126
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2025-04-13 14:02:44 +0100

    Update CountryInfo_Asia.cs

--- src/PhoneNumbers/CountryInfo_Asia.cs
+++ src/PhoneNumbers/CountryInfo_Asia.cs
@@ -63,4 +63,19 @@ public partial class CountryInfo
         NsnLengths = new ReadOnlyCollection<int>([7, 10]),
         TrunkPrefix = "0",
     };
+
+    /// <summary>
+    /// Gets the <see cref="CountryInfo"/> for United Arab Emirates.
+    /// </summary>
+    public static CountryInfo UnitedArabEmirates { get; } = new()
+    {
+        CallingCode = "971",
+        Continent = Asia,
+        FullName = "United Arab Emirates",
+        Iso3166Code = "AE",
+        Name = "United Arab Emirates",
+        NdcLengths = new ReadOnlyCollection<int>([3, 2]),
+        NsnLengths = new ReadOnlyCollection<int>([8, 9]),
+        TrunkPrefix = "0",
+    };
 }
