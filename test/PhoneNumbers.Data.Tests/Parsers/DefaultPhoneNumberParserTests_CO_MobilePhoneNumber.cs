caf64f79ee3bbb11511eafb17c951a69a36258ad
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2025-04-21 09:39:58 +0000

    Colombia

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_CO_MobilePhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_CO_MobilePhoneNumber.cs
@@ -16,97 +16,17 @@ public class DefaultPhoneNumberParserTests_CO_MobilePhoneNumber
     [InlineData("3049999999", "304", "9999999")]
     [InlineData("3052000000", "305", "2000000")]
     [InlineData("3059999999", "305", "9999999")]
-    public void Parse_Known_MobilePhoneNumber_30X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
-    {
-        var parseResult = s_parser.Parse(value);
-        parseResult.ThrowIfFailure();
-
-        var phoneNumber = parseResult.PhoneNumber;
-
-        Assert.NotNull(phoneNumber);
-        Assert.IsType<MobilePhoneNumber>(phoneNumber);
-
-        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
-        Assert.Equal(CountryInfo.Colombia, mobilePhoneNumber.Country);
-        Assert.False(mobilePhoneNumber.IsPager);
-        Assert.False(mobilePhoneNumber.IsVirtual);
-        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
-        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
-    }
-
-    [Theory]
     [InlineData("3102000000", "310", "2000000")]
     [InlineData("3109999999", "310", "9999999")]
-    [InlineData("3142000000", "314", "2000000")]
-    [InlineData("3149999999", "314", "9999999")]
-    [InlineData("3152000000", "315", "2000000")]
-    [InlineData("3159999999", "315", "9999999")]
-    public void Parse_Known_MobilePhoneNumber_31X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
-    {
-        var parseResult = s_parser.Parse(value);
-        parseResult.ThrowIfFailure();
-
-        var phoneNumber = parseResult.PhoneNumber;
-
-        Assert.NotNull(phoneNumber);
-        Assert.IsType<MobilePhoneNumber>(phoneNumber);
-
-        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
-        Assert.Equal(CountryInfo.Colombia, mobilePhoneNumber.Country);
-        Assert.False(mobilePhoneNumber.IsPager);
-        Assert.False(mobilePhoneNumber.IsVirtual);
-        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
-        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
-    }
-
-    [Theory]
     [InlineData("3242000000", "324", "2000000")]
     [InlineData("3249999999", "324", "9999999")]
-    public void Parse_Known_MobilePhoneNumber_32X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
-    {
-        var parseResult = s_parser.Parse(value);
-        parseResult.ThrowIfFailure();
-
-        var phoneNumber = parseResult.PhoneNumber;
-
-        Assert.NotNull(phoneNumber);
-        Assert.IsType<MobilePhoneNumber>(phoneNumber);
-
-        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
-        Assert.Equal(CountryInfo.Colombia, mobilePhoneNumber.Country);
-        Assert.False(mobilePhoneNumber.IsPager);
-        Assert.False(mobilePhoneNumber.IsVirtual);
-        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
-        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
-    }
-
-    [Theory]
     [InlineData("3332000000", "333", "2000000")]
     [InlineData("3339999999", "333", "9999999")]
-    public void Parse_Known_MobilePhoneNumber_33X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
-    {
-        var parseResult = s_parser.Parse(value);
-        parseResult.ThrowIfFailure();
-
-        var phoneNumber = parseResult.PhoneNumber;
-
-        Assert.NotNull(phoneNumber);
-        Assert.IsType<MobilePhoneNumber>(phoneNumber);
-
-        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
-        Assert.Equal(CountryInfo.Colombia, mobilePhoneNumber.Country);
-        Assert.False(mobilePhoneNumber.IsPager);
-        Assert.False(mobilePhoneNumber.IsVirtual);
-        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
-        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
-    }
-
-    [Theory]
     [InlineData("3502000000", "350", "2000000")]
     [InlineData("3509999999", "350", "9999999")]
     [InlineData("3512000000", "351", "2000000")]
     [InlineData("3519999999", "351", "9999999")]
-    public void Parse_Known_MobilePhoneNumber_35X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    public void Parse_Known_MobilePhoneNumber(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
         parseResult.ThrowIfFailure();
