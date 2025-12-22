a2664d44ae9ca189a3f1644491504dab86dc2c75
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2025-12-22 13:48:13 +0000

    Update IE Numbers

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_IE_MobilePhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_IE_MobilePhoneNumber.cs
@@ -10,6 +10,8 @@ public class DefaultPhoneNumberParserTests_IE_MobilePhoneNumber
     [Theory]
     [InlineData("0830000000", "83", "0000000")]
     [InlineData("0839999999", "83", "9999999")]
+    [InlineData("0850000000", "85", "0000000")]
+    [InlineData("0859999999", "85", "9999999")]
     [InlineData("0870000000", "87", "0000000")]
     [InlineData("0879999999", "87", "9999999")]
     [InlineData("0890000000", "89", "0000000")]
@@ -28,48 +30,6 @@ public class DefaultPhoneNumberParserTests_IE_MobilePhoneNumber
         Assert.Equal(CountryInfo.Ireland, mobilePhoneNumber.Country);
         Assert.False(mobilePhoneNumber.IsPager);
         Assert.False(mobilePhoneNumber.IsVirtual);
-        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
-        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
-    }
-
-    [Theory]
-    [InlineData("0822000000", "822", "000000")]
-    [InlineData("0822999999", "822", "999999")]
-    public void Parse_Known_MobilePhoneNumber_Pager(string value, string NationalDestinationCode, string subscriberNumber)
-    {
-        var parseResult = s_parser.Parse(value);
-        parseResult.ThrowIfFailure();
-
-        var phoneNumber = parseResult.PhoneNumber;
-
-        Assert.NotNull(phoneNumber);
-        Assert.IsType<MobilePhoneNumber>(phoneNumber);
-
-        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
-        Assert.Equal(CountryInfo.Ireland, mobilePhoneNumber.Country);
-        Assert.True(mobilePhoneNumber.IsPager);
-        Assert.False(mobilePhoneNumber.IsVirtual);
-        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
-        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
-    }
-
-    [Theory]
-    [InlineData("0700000000", "700", "000000")]
-    [InlineData("0700999999", "700", "999999")]
-    public void Parse_Known_MobilePhoneNumber_Virtual(string value, string NationalDestinationCode, string subscriberNumber)
-    {
-        var parseResult = s_parser.Parse(value);
-        parseResult.ThrowIfFailure();
-
-        var phoneNumber = parseResult.PhoneNumber;
-
-        Assert.NotNull(phoneNumber);
-        Assert.IsType<MobilePhoneNumber>(phoneNumber);
-
-        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
-        Assert.Equal(CountryInfo.Ireland, mobilePhoneNumber.Country);
-        Assert.False(mobilePhoneNumber.IsPager);
-        Assert.True(mobilePhoneNumber.IsVirtual);
         Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
         Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
     }
--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_IE_NonGeographicPhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_IE_NonGeographicPhoneNumber.cs
@@ -8,9 +8,9 @@ public class DefaultPhoneNumberParserTests_IE_NonGeographicPhoneNumber
     private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.Ireland);
 
     [Theory]
-    [InlineData("0800000000", "800", "000000")]
-    [InlineData("0800999999", "800", "999999")]
-    public void Parse_Known_NonGeographicPhoneNumber_Freephone(string value, string NationalDestinationCode, string subscriberNumber)
+    [InlineData("0818000000", "818", "000000")]
+    [InlineData("0818999999", "818", "999999")]
+    public void Parse_Known_NonGeographicPhoneNumber_8XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
         parseResult.ThrowIfFailure();
@@ -22,7 +22,7 @@ public class DefaultPhoneNumberParserTests_IE_NonGeographicPhoneNumber
 
         var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
         Assert.Equal(CountryInfo.Ireland, nonGeographicPhoneNumber.Country);
-        Assert.True(nonGeographicPhoneNumber.IsFreephone);
+        Assert.False(nonGeographicPhoneNumber.IsFreephone);
         Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
         Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
         Assert.False(nonGeographicPhoneNumber.IsSharedCost);
@@ -49,60 +49,6 @@ public class DefaultPhoneNumberParserTests_IE_NonGeographicPhoneNumber
         Assert.True(nonGeographicPhoneNumber.IsMachineToMachine);
         Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
         Assert.False(nonGeographicPhoneNumber.IsSharedCost);
-        Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
-        Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
-    }
-
-    [Theory]
-    [InlineData("015120000", "151", "20000")]
-    [InlineData("015189999", "151", "89999")]
-    [InlineData("015200000", "152", "00000")]
-    [InlineData("015209999", "152", "09999")]
-    [InlineData("015900000", "159", "00000")]
-    [InlineData("015909999", "159", "09999")]
-    [InlineData("015980000", "159", "80000")]
-    [InlineData("015999999", "159", "99999")]
-    public void Parse_Known_NonGeographicPhoneNumber_PremiumRate(string value, string NationalDestinationCode, string subscriberNumber)
-    {
-        var parseResult = s_parser.Parse(value);
-        parseResult.ThrowIfFailure();
-
-        var phoneNumber = parseResult.PhoneNumber;
-
-        Assert.NotNull(phoneNumber);
-        Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
-
-        var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
-        Assert.Equal(CountryInfo.Ireland, nonGeographicPhoneNumber.Country);
-        Assert.False(nonGeographicPhoneNumber.IsFreephone);
-        Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
-        Assert.True(nonGeographicPhoneNumber.IsPremiumRate);
-        Assert.False(nonGeographicPhoneNumber.IsSharedCost);
-        Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
-        Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
-    }
-
-    [Theory]
-    [InlineData("018500000", "185", "00000")]
-    [InlineData("018509999", "185", "09999")]
-    [InlineData("018900000", "189", "00000")]
-    [InlineData("018909999", "189", "09999")]
-    public void Parse_Known_NonGeographicPhoneNumber_SharedCost(string value, string NationalDestinationCode, string subscriberNumber)
-    {
-        var parseResult = s_parser.Parse(value);
-        parseResult.ThrowIfFailure();
-
-        var phoneNumber = parseResult.PhoneNumber;
-
-        Assert.NotNull(phoneNumber);
-        Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
-
-        var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
-        Assert.Equal(CountryInfo.Ireland, nonGeographicPhoneNumber.Country);
-        Assert.False(nonGeographicPhoneNumber.IsFreephone);
-        Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
-        Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
-        Assert.True(nonGeographicPhoneNumber.IsSharedCost);
         Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
         Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
     }
