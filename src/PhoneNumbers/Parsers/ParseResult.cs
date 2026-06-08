22c14ada7f877171e4b593f29ef5cee5b2955832
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2026-06-08 10:33:42 +0100

    Simplify ParseResult as a record

--- src/PhoneNumbers/Parsers/ParseResult.cs
+++ src/PhoneNumbers/Parsers/ParseResult.cs
@@ -3,27 +3,15 @@ namespace PhoneNumbers.Parsers;
 /// <summary>
 /// A class which represents the result of a phone number parse attempt.
 /// </summary>
-internal sealed class ParseResult
+internal sealed record ParseResult(string? ParseError, PhoneNumber? PhoneNumber)
 {
-    private ParseResult() { }
-
-    /// <summary>
-    /// Gets the error which resulted caused the parse attempt to fail.
-    /// </summary>
-    internal string? ParseError { get; init; }
-
-    /// <summary>
-    /// Gets the <see cref="PhoneNumber"/> which was successfully parsed.
-    /// </summary>
-    internal PhoneNumber? PhoneNumber { get; init; }
-
     /// <summary>
     /// Creates a <see cref="ParseResult"/> which failed.
     /// </summary>
     /// <param name="parseError">The error which resulted caused the parse attempt to fail.</param>
     /// <returns>A <see cref="ParseResult"/> for the parse error.</returns>
     internal static ParseResult Failure(string parseError) =>
-        new() { ParseError = parseError };
+        new(parseError, PhoneNumber: null);
 
     /// <summary>
     /// Creates a <see cref="ParseResult"/> which succeeded.
@@ -31,7 +19,7 @@ internal sealed class ParseResult
     /// <param name="phoneNumber">The <see cref="PhoneNumber"/> which was successfully parsed.</param>
     /// <returns>A <see cref="ParseResult"/> for the parse success.</returns>
     internal static ParseResult Success(PhoneNumber phoneNumber) =>
-        new() { PhoneNumber = phoneNumber };
+        new(ParseError: null, PhoneNumber: phoneNumber);
 
     /// <summary>
     /// Throws a <see cref="ParseException"/> with the parse error if the parse operation failed.
