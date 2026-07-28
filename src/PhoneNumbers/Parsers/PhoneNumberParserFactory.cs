48b67b3438f8d6b614087627a6c7e62998112e5b
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2026-07-28 08:41:45 +0100

    Avoid reflection cost

--- src/PhoneNumbers/Parsers/PhoneNumberParserFactory.cs
+++ src/PhoneNumbers/Parsers/PhoneNumberParserFactory.cs
@@ -1,5 +1,4 @@
 using System.Collections.Concurrent;
-using System.Reflection;
 
 namespace PhoneNumbers.Parsers;
 
@@ -7,21 +6,20 @@ internal sealed class PhoneNumberParserFactory
 {
     private readonly ConcurrentDictionary<CountryInfo, PhoneNumberParser> _parserCache = new();
 
-    internal PhoneNumberParser GetParser(CountryInfo countryInfo) =>
-        _parserCache.GetOrAdd(
+    internal PhoneNumberParser GetParser(CountryInfo countryInfo)
+    {
+        ArgumentNullException.ThrowIfNull(countryInfo);
+
+        return _parserCache.GetOrAdd(
             countryInfo,
             x =>
             {
-                var createMethod = typeof(PhoneNumberParser)
-                    .Assembly
-                    .GetType($"PhoneNumbers.Parsers.{x.Iso3166Code}PhoneNumberParser")
-                    ?.GetMethod("Create", BindingFlags.Public | BindingFlags.Static);
-
-                if (createMethod?.Invoke(obj: null, parameters: null) is PhoneNumberParser parser)
+                if (x == CountryInfo.UnitedKingdom)
                 {
-                    return parser;
+                    return GBPhoneNumberParser.Create();
                 }
 
                 return DefaultPhoneNumberParser.Create(x);
             });
+    }
 }
