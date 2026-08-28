2a5c04dff0c3bf3224a4512add6a1108830737ae
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2026-08-28 14:12:44 +0100

    Update RO numbers

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_RO_MobilePhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_RO_MobilePhoneNumber.cs
@@ -8,14 +8,10 @@ public class DefaultPhoneNumberParserTests_RO_MobilePhoneNumber
     private static readonly PhoneNumberParser s_parser = DefaultPhoneNumberParser.Create(CountryInfo.Romania);
 
     [Theory]
-    [InlineData("0610000000", "61", "0000000")]
-    [InlineData("0619999999", "61", "9999999")]
+    [InlineData("0600000000", "60", "0000000")]
+    [InlineData("0609999999", "60", "9999999")]
     [InlineData("0690000000", "69", "0000000")]
     [InlineData("0699999999", "69", "9999999")]
-    [InlineData("0710000000", "71", "0000000")]
-    [InlineData("0719999999", "71", "9999999")]
-    [InlineData("0790000000", "79", "0000000")]
-    [InlineData("0799999999", "79", "9999999")]
     public void Parse_Known_MobilePhoneNumber(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
@@ -35,8 +31,6 @@ public class DefaultPhoneNumberParserTests_RO_MobilePhoneNumber
     }
 
     [Theory]
-    [InlineData("0600000000", "60", "0000000")]
-    [InlineData("0609999999", "60", "9999999")]
     [InlineData("0700000000", "70", "0000000")]
     [InlineData("0709999999", "70", "9999999")]
     public void Parse_Known_MobilePhoneNumber_Virtual(string value, string NationalDestinationCode, string subscriberNumber)
