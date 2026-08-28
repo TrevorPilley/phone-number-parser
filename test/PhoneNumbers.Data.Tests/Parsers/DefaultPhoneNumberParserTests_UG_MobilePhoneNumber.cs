ec9849f8578ae0cf24d694484108779f98c8dc87
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2026-08-28 15:24:01 +0100

    Update UG numbers

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_UG_MobilePhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_UG_MobilePhoneNumber.cs
@@ -10,21 +10,126 @@ public class DefaultPhoneNumberParserTests_UG_MobilePhoneNumber
     [Theory]
     [InlineData("0700000000", "700", "000000")]
     [InlineData("0700999999", "700", "999999")]
+    public void Parse_Known_MobilePhoneNumber_70X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Uganda, mobilePhoneNumber.Country);
+        Assert.False(mobilePhoneNumber.IsPager);
+        Assert.False(mobilePhoneNumber.IsVirtual);
+        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
     [InlineData("0719000000", "719", "000000")]
     [InlineData("0719999999", "719", "999999")]
+    public void Parse_Known_MobilePhoneNumber_71X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Uganda, mobilePhoneNumber.Country);
+        Assert.False(mobilePhoneNumber.IsPager);
+        Assert.False(mobilePhoneNumber.IsVirtual);
+        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
+    [InlineData("0721000000", "721", "000000")]
+    [InlineData("0721099999", "721", "099999")]
+    [InlineData("0724000000", "724", "000000")]
+    [InlineData("0724999999", "724", "999999")]
+    [InlineData("0726000000", "726", "000000")]
+    [InlineData("0726999999", "726", "999999")]
+    [InlineData("0727000000", "727", "000000")]
+    [InlineData("0727999999", "727", "999999")]
+    [InlineData("0728000000", "728", "000000")]
+    [InlineData("0728099999", "728", "099999")]
+    public void Parse_Known_MobilePhoneNumber_72X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Uganda, mobilePhoneNumber.Country);
+        Assert.False(mobilePhoneNumber.IsPager);
+        Assert.False(mobilePhoneNumber.IsVirtual);
+        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
+    [InlineData("0730000000", "730", "000000")]
+    [InlineData("0730999999", "730", "999999")]
+    [InlineData("0731000000", "731", "000000")]
+    [InlineData("0731999999", "731", "999999")]
+    [InlineData("0734000000", "734", "000000")]
+    [InlineData("0734099999", "734", "099999")]
+    [InlineData("0737000000", "737", "000000")]
+    [InlineData("0737099999", "737", "099999")]
+    public void Parse_Known_MobilePhoneNumber_73X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Uganda, mobilePhoneNumber.Country);
+        Assert.False(mobilePhoneNumber.IsPager);
+        Assert.False(mobilePhoneNumber.IsVirtual);
+        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
     [InlineData("0740000000", "740", "000000")]
     [InlineData("0740999999", "740", "999999")]
-    [InlineData("0746000000", "746", "000000")]
-    [InlineData("0746999999", "746", "999999")]
-    [InlineData("0750000000", "750", "000000")]
-    [InlineData("0750999999", "750", "999999")]
-    [InlineData("0768000000", "768", "000000")]
-    [InlineData("0768999999", "768", "999999")]
-    [InlineData("0770000000", "770", "000000")]
-    [InlineData("0770999999", "770", "999999")]
-    [InlineData("0789000000", "789", "000000")]
-    [InlineData("0789999999", "789", "999999")]
-    public void Parse_Known_MobilePhoneNumber(string value, string NationalDestinationCode, string subscriberNumber)
+    public void Parse_Known_MobilePhoneNumber_74X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<MobilePhoneNumber>(phoneNumber);
+
+        var mobilePhoneNumber = (MobilePhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Uganda, mobilePhoneNumber.Country);
+        Assert.False(mobilePhoneNumber.IsPager);
+        Assert.False(mobilePhoneNumber.IsVirtual);
+        Assert.Equal(NationalDestinationCode, mobilePhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, mobilePhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
+    [InlineData("0795000000", "795", "000000")]
+    [InlineData("0795999999", "795", "999999")]
+    public void Parse_Known_MobilePhoneNumber_79X_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
         parseResult.ThrowIfFailure();
