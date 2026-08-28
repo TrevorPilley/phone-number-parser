e13460e395d9dd76c9a713cafc38944ef2c49fa2
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2026-08-28 12:47:51 +0100

    Update PT numbers

--- test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_PT_NonGeographicPhoneNumber.cs
+++ test/PhoneNumbers.Data.Tests/Parsers/DefaultPhoneNumberParserTests_PT_NonGeographicPhoneNumber.cs
@@ -33,10 +33,67 @@ public class DefaultPhoneNumberParserTests_PT_NonGeographicPhoneNumber
     }
 
     [Theory]
-    [InlineData("700000000", "700", "000000")]
-    [InlineData("700999999", "700", "999999")]
-    [InlineData("799000000", "799", "000000")]
-    [InlineData("799999999", "799", "999999")]
+    [InlineData("600000000", "600", "000000")]
+    [InlineData("600999999", "600", "999999")]
+    [InlineData("601000000", "601", "000000")]
+    [InlineData("601999999", "601", "999999")]
+    [InlineData("607000000", "607", "000000")]
+    [InlineData("607999999", "607", "999999")]
+    [InlineData("609000000", "609", "000000")]
+    [InlineData("609999999", "609", "999999")]
+    [InlineData("610000000", "610", "000000")]
+    [InlineData("610999999", "610", "999999")]
+    [InlineData("619000000", "619", "000000")]
+    [InlineData("619999999", "619", "999999")]
+    [InlineData("639000000", "639", "000000")]
+    [InlineData("639999999", "639", "999999")]
+    [InlineData("646000000", "646", "000000")]
+    [InlineData("646999999", "646", "999999")]
+    [InlineData("659000000", "659", "000000")]
+    [InlineData("659999999", "659", "999999")]
+    [InlineData("669000000", "669", "000000")]
+    [InlineData("669999999", "669", "999999")]
+    [InlineData("670000000", "670", "000000")]
+    [InlineData("670999999", "670", "999999")]
+    [InlineData("690000000", "690", "000000")]
+    [InlineData("690999999", "690", "999999")]
+    public void Parse_Known_NonGeographicPhoneNumber_6XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
+    {
+        var parseResult = s_parser.Parse(value);
+        parseResult.ThrowIfFailure();
+
+        var phoneNumber = parseResult.PhoneNumber;
+
+        Assert.NotNull(phoneNumber);
+        Assert.IsType<NonGeographicPhoneNumber>(phoneNumber);
+
+        var nonGeographicPhoneNumber = (NonGeographicPhoneNumber)phoneNumber;
+        Assert.Equal(CountryInfo.Portugal, nonGeographicPhoneNumber.Country);
+        Assert.False(nonGeographicPhoneNumber.IsFreephone);
+        Assert.False(nonGeographicPhoneNumber.IsMachineToMachine);
+        Assert.False(nonGeographicPhoneNumber.IsPremiumRate);
+        Assert.False(nonGeographicPhoneNumber.IsSharedCost);
+        Assert.Equal(NationalDestinationCode, nonGeographicPhoneNumber.NationalDestinationCode);
+        Assert.Equal(subscriberNumber, nonGeographicPhoneNumber.SubscriberNumber);
+    }
+
+    [Theory]
+    [InlineData("703000000", "703", "000000")]
+    [InlineData("703999999", "703", "999999")]
+    [InlineData("705000000", "705", "000000")]
+    [InlineData("705999999", "705", "999999")]
+    [InlineData("707000000", "707", "000000")]
+    [InlineData("707999999", "707", "999999")]
+    [InlineData("708000000", "708", "000000")]
+    [InlineData("708999999", "708", "999999")]
+    [InlineData("710000000", "710", "000000")]
+    [InlineData("710999999", "710", "999999")]
+    [InlineData("719000000", "719", "000000")]
+    [InlineData("719999999", "719", "999999")]
+    [InlineData("760000000", "760", "000000")]
+    [InlineData("760999999", "760", "999999")]
+    [InlineData("762000000", "762", "000000")]
+    [InlineData("762999999", "762", "999999")]
     public void Parse_Known_NonGeographicPhoneNumber_7XX_NationalDestinationCode(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
@@ -83,6 +140,8 @@ public class DefaultPhoneNumberParserTests_PT_NonGeographicPhoneNumber
     [Theory]
     [InlineData("800000000", "800", "000000")]
     [InlineData("800999999", "800", "999999")]
+    [InlineData("802000000", "802", "000000")]
+    [InlineData("802999999", "802", "999999")]
     public void Parse_Known_NonGeographicPhoneNumber_Freephone(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
@@ -104,8 +163,8 @@ public class DefaultPhoneNumberParserTests_PT_NonGeographicPhoneNumber
     }
 
     [Theory]
-    [InlineData("607000000", "607", "000000")]
-    [InlineData("607999999", "607", "999999")]
+    [InlineData("648000000", "648", "000000")]
+    [InlineData("648999999", "648", "999999")]
     public void Parse_Known_NonGeographicPhoneNumber_PremiumRate(string value, string NationalDestinationCode, string subscriberNumber)
     {
         var parseResult = s_parser.Parse(value);
